51967648ff7974619581422825908d5cd1547392
Chen Bin <chenbin.sh@gmail.com>
2014-08-15 18:31:42 +1000

    fixed minor error in ruby setup

--- init-ruby-mode.el
+++ init-ruby-mode.el
@@ -1,27 +1,21 @@
 (add-auto-mode 'ruby-mode "\\.rb\\'" "Rakefile\\'" "\.rake\\'" "\.rxml\\'" "\.rjs\\'" ".irbrc\\'" "\.builder\\'" "\.ru\\'" "\.gemspec\\'" "Gemfile\\'")
 
-
 (autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
-
 (setq ruby-use-encoding-map nil)
 
-(add-hook 'robe-mode-hook
+(add-hook 'ruby-mode-hook
           (lambda ()
-            ;; {{ rails stuff
             (require 'rinari)
-            (setq compile-command "rake ")
             (robe-mode)
+            (push 'company-robe company-backends)
+            (setq compile-command "rake ")
             (flymake-ruby-load)))
 
 ;; doc look up
-(defalias 'ri 'yari-helm)
-
 (autoload 'yari-helm "yari" "" t nil)
 (autoload 'yari "yari" "" t nil)
+(defalias 'ri 'yari-helm)
 
-;;----------------------------------------------------------------------------
-;; Ruby - erb
-;;----------------------------------------------------------------------------
 (add-to-list 'auto-mode-alist '("\\.rhtml\\(\\.erb\\)?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jst\\.ejs\\'"  . web-mode))
 
@@ -29,6 +23,5 @@
   (interactive)
   (let ((default-directory (or (rinari-root) default-directory)))
     (shell-command (concat "ctags -a -e -f " rinari-tags-file-name " --tag-relative -R app lib vendor test"))))
-;; }}
 
 (provide 'init-ruby-mode)
