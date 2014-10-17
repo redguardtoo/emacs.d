a7bd4e9553cd6568156d388b6ada091d75d4d0ea
lzh <lizhaohui1991@gmail.com>
2014-10-18 00:06:38 +0800

    specify file location of Proofgeneral

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -982,6 +982,6 @@ The full path into relative path insert it as a local file link in org-mode"
 ;; }}
 
 ;; coq programming language
-(load-file "/home/cb/.emacs.d/site-lisp/ProofGeneral-4.2/generic/proof-site.el")
+(load-file "~/.emacs.d/site-lisp/ProofGeneral-4.2/generic/proof-site.el")
 
 (provide 'init-misc)
