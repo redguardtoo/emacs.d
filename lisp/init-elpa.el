270d166b087b8fedc8a5a58ca80fbadc17b9d7ab
Chen Bin <chenbin.sh@gmail.com>
2014-10-24 19:46:34 +1100

    melpa use new domain

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -75,8 +75,8 @@ ARCHIVE is the string name of the package archive.")
 
 ;; well, melpa does not bother supporting emacs23 any more, but cl-lib is still required
 ;; TODO: in half a year, I will remove gnu elpa because emacs 24.3 is the minimum version
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")
+                         ("melpa-stable" . "http://melpa-stable.org/packages/")
                          ))
 (if (not *emacs24*) (add-to-list 'package-archives '("localelpa" . "~/.emacs.d/localelpa")))
 
