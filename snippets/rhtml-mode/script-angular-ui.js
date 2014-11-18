8784aff016f55beca534b630a7083ab1839152c9
Chen Bin <chenbin.sh@gmail.com>
2014-11-18 11:00:02 +1100

    angular snippets

---
+++ snippets/rhtml-mode/script-angular-ui.js
@@ -0,0 +1,6 @@
+# -*- mode: snippet -*-
+# name: <script src="angularui.js">...</script>
+# key: script
+# contributor: Chen Bin <chenbin DOT sh AT gmail>
+# --
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.11.2/ui-bootstrap-tpls${1:.min}.js"></script>
--- snippets/rhtml-mode/script-angular.js
+++ snippets/rhtml-mode/script-angular.js
@@ -3,4 +3,4 @@
 # key: script
 # contributor: Chen Bin <chenbin DOT sh AT gmail>
 # --
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.16/angular${1:.min}.js"></script>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular${1:.min}.js"></script>
