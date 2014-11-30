864d600d06ff760ec091e5c833ea77e8dd541cec
Chen Bin <chenbin.sh@gmail.com>
2014-11-30 16:39:07 +1100

    fixed minor issue in legacy code

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -137,6 +137,17 @@
 (global-set-key (kbd "M-k") 'keyboard-quit)
 (global-set-key (kbd "C-r") 'undo-tree-redo)
 
+;; esc quits
+(defun minibuffer-keyboard-quit ()
+  "Abort recursive edit.
+In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+  (interactive)
+  (if (and delete-selection-mode transient-mark-mode mark-active)
+      (setq deactivate-mark t)
+    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+    (abort-recursive-edit)))
+
 (define-key evil-normal-state-map [escape] 'keyboard-quit)
 (define-key evil-visual-state-map [escape] 'keyboard-quit)
 (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
