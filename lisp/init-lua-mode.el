0c90a891e7bc95232154b27c5d5804c7c54b87a5
Chen Bin <chenbin.sh@gmail.com>
2014-12-05 16:44:53 +1100

    tweak lua setup

--- lisp/init-lua-mode.el
+++ lisp/init-lua-mode.el
@@ -1,10 +1,10 @@
-(add-hook 'lua-mode-hook
-          (lambda ()
-            (setq safe-local-variable-values
-                  '((lua-indent-level . 2)
-                    (lua-indent-level . 3)
-                    (lua-indent-level . 4)
-                    (lua-indent-level . 8)))
-            (flymake-lua-load)
-            ))
+(defun my-lua-mode-setup ()
+  (setq safe-local-variable-values
+        '((lua-indent-level . 2)
+          (lua-indent-level . 3)
+          (lua-indent-level . 4)
+          (lua-indent-level . 8)))
+  (flymake-lua-load))
+
+(add-hook 'lua-mode-hook 'my-lua-mode-setup)
 (provide 'init-lua-mode)
