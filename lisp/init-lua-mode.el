6021f780ca2d68377d565d3ddaef7d99a46ba24c
Chen Bin <chenbin.sh@gmail.com>
2014-12-05 20:19:31 +1100

    better imenu in lua-mode

--- lisp/init-lua-mode.el
+++ lisp/init-lua-mode.el
@@ -1,4 +1,9 @@
 (defun my-lua-mode-setup ()
+  (interactive)
+  (setq-local imenu-generic-expression '(("Variable" "^ *\\([a-zA-Z0-9_.]+\\) *= *{ *[^ ]*$" 1)
+                                         ("Function" "function +\\([^ (]+\\).*$" 1)
+                                         ("Module" "^ *module +\\([^ ]+\\) *$" 1)
+                                         ("Variable" "^ *local +\\([^ ]+\\).*$" 1)))
   (setq safe-local-variable-values
         '((lua-indent-level . 2)
           (lua-indent-level . 3)
