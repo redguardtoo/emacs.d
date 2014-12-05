928fae2f4ae89542c070dcd362b1fd633f50672b
Chen Bin <chenbin.sh@gmail.com>
2014-12-05 17:54:42 +1100

    minor color-theme issue

--- init.el
+++ init.el
@@ -130,16 +130,17 @@
 (require 'init-hs-minor-mode)
 
 
-(setq color-theme-illegal-faces "^\\(w3-\\|dropdown-\\|info-\\|linum\\|yas-\\|font-lock\\)")
+;; misc has some crucial tools I need immediately
+(require 'init-misc)
+
 ;; color theme
 (require 'color-theme)
 (require 'color-theme-molokai)
 (color-theme-molokai)
+;; This line must be after color-theme-molokai! Don't know why.
+(setq color-theme-illegal-faces "^\\(w3-\\|dropdown-\\|info-\\|linum\\|yas-\\|font-lock\\)")
 ;; (color-theme-select 'color-theme-xp)
 ;; (color-theme-xp)
-
-;; misc has some crucial tools I need immediately
-(require 'init-misc)
 
 (setq idle-require-idle-delay 3)
 (setq idle-require-symbols '(init-writting
