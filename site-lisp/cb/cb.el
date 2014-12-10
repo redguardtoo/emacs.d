5ece0e2926ffb58aebe79fae005b4f9ef74eb1b7
Chen Bin <chenbin.sh@gmail.com>
2014-12-10 21:58:34 +1100

    improve cb.el

--- site-lisp/cb/cb.el
+++ site-lisp/cb/cb.el
@@ -9,13 +9,33 @@
 
 ;; This file is not part of GNU Emacs.
 
+;;; Code:
+
+(defvar cb-app-url-prefix "http://127.0.0.1:8001/")
+
 (defun cb--parent-dirname (dir)
   (cadr (nreverse (split-string dir "/"))))
 
 (defun cb--root-dir ()
   (locate-dominating-file (file-name-as-directory (file-name-directory buffer-file-name)) ".git"))
 
-;;; Code:
+(defun cb--copy-yank-str (msg &optional clipboard-only)
+  (unless clipboard-only (kill-new msg))
+  (cond
+   ;; display-graphic-p need windows 23.3.1
+   ((and (display-graphic-p) x-select-enable-clipboard)
+    (x-set-selection 'CLIPBOARD msg))
+   (t (with-temp-buffer
+        (insert msg)
+        (shell-command-on-region (point-min) (point-max)
+                                 (cond
+                                  ((eq system-type 'cygwin) "putclip")
+                                  ((eq system-type 'darwin) "pbcopy")
+                                  (t "xsel -ib")
+                                  )))
+    )))
+
+;;;###autoload
 (defun cb-switch-between-controller-and-view ()
   (interactive)
   (let (f)
@@ -51,6 +71,35 @@
         (goto-char (point-min))
         (re-search-forward (concat "^" wf " *("))
         ))
+    ))
+
+(defun cb-get-url-from-controller ()
+  (interactive)
+  (let (f)
+    (setq f (file-name-nondirectory (buffer-file-name)))
+    ;; open the html of controller
+    (when (string= (file-name-extension f) "erl")
+      (let (namespace ctrlname type info len wf paras-cnt)
+        (setq info (split-string f "_"))
+        (setq len (length info))
+        (when (and (> len 2) (which-function))
+          (setq wf (which-function))
+          (setq ctrlname (nth (- len 2) info))
+          (setq type (file-name-base (nth (- len 1) info)))
+          (if (string-match ".*\/\\([0-9]+\\)$" wf)
+            (setq paras-cnt (match-string 1 wf)))
+          (let ((app-url (concat (file-name-as-directory cb-app-url-prefix)
+                                 ctrlname
+                                 "/"
+                                 (replace-regexp-in-string "/[0-9]+$" "" (which-function))
+                                 "/"
+                                 (if (and (string= paras-cnt "1") (string= paras-cnt "0"))
+                                     ""
+                                     paras-cnt)
+                                 )))
+            (cb--copy-yank-str app-url)
+            (message "%s => clipboard" app-url))
+          )))
     ))
 
 (provide 'cb)
