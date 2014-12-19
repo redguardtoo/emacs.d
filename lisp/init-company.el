582ade5b36024e6b8311b546667c4b9ca1539e20
Chen Bin <chenbin.sh@gmail.com>
2014-12-19 15:16:45 +0800

    #231 cider need this

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -20,7 +20,7 @@
      (add-to-list 'company-backends 'company-c-headers)
      ;; can't work with TRAMP
      (setq company-backends (delete 'company-ropemacs company-backends))
-     (setq company-backends (delete 'company-capf company-backends))
+     ;; (setq company-backends (delete 'company-capf company-backends))
      ;; I don't like the downcase word in company-dabbrev
      ;; for languages use camel case naming convention
      (setq company-dabbrev-downcase nil)
