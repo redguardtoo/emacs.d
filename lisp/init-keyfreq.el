7e0aa948fd15a81b588c5e4c3556b71475ad9d43
Chen Bin <chenbin.sh@gmail.com>
2015-01-19 12:55:09 +1100

    keyfreq minor stuff

--- lisp/init-keyfreq.el
+++ lisp/init-keyfreq.el
@@ -1,5 +1,7 @@
 (require 'keyfreq)
 (keyfreq-mode 1)
 (keyfreq-autosave-mode 1)
+(unless (file-exists-p keyfreq-file)
+  (write-region "" nil keyfreq-file))
 ;; And use keyfreq-show to see how many times you used a command.
 (provide 'init-keyfreq)
