edfbae0a7043221ffbc6bcfd23feb0bc38b51d79
Chen Bin <chenbin.sh@gmail.com>
2015-02-04 13:42:06 +1100

    clean code

--- init.el
+++ init.el
@@ -182,13 +182,6 @@
 ;;----------------------------------------------------------------------------
 (require 'init-locales)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold))) t))
-
 ;;; Local Variables:
 ;;; no-byte-compile: t
 ;;; End:
