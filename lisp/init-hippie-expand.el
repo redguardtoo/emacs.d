73f3483eaa82b4b918d5206f5e7c10416ed38e3d
Chen Bin <chenbin.sh@gmail.com>
2015-03-27 22:19:58 +1100

    enable hippie-expand by dict in all the modes

--- lisp/init-hippie-expand.el
+++ lisp/init-hippie-expand.el
@@ -3,26 +3,24 @@
 ;; The actual expansion function
 (defun try-expand-by-dict (old)
   ;; old is true if we have already attempted an expansion
-  (when (memq major-mode '(org-mode
-                           message-mode))
-    (unless (bound-and-true-p ispell-minor-mode)
-      (ispell-minor-mode 1))
-    (let ((lookup-func (if (fboundp 'ispell-lookup-words)
-                           'ispell-lookup-words
-                         'lookup-words)))
-      (unless old
-        (he-init-string (he-lisp-symbol-beg) (point))
-        (if (not (he-string-member he-search-string he-tried-table))
-            (setq he-tried-table (cons he-search-string he-tried-table)))
-        (setq he-expand-list
-              (and (not (equal he-search-string ""))
-                   (funcall lookup-func (concat (buffer-substring-no-properties (he-lisp-symbol-beg) (point)) "*")))))
-      (if (null he-expand-list)
-          (if old (he-reset-string))
-        (he-substitute-string (car he-expand-list))
-        (setq he-expand-list (cdr he-expand-list))
-        t)
-      )))
+  (unless (bound-and-true-p ispell-minor-mode)
+    (ispell-minor-mode 1))
+  (let ((lookup-func (if (fboundp 'ispell-lookup-words)
+                       'ispell-lookup-words
+                       'lookup-words)))
+    (unless old
+      (he-init-string (he-lisp-symbol-beg) (point))
+      (if (not (he-string-member he-search-string he-tried-table))
+        (setq he-tried-table (cons he-search-string he-tried-table)))
+      (setq he-expand-list
+            (and (not (equal he-search-string ""))
+                 (funcall lookup-func (concat (buffer-substring-no-properties (he-lisp-symbol-beg) (point)) "*")))))
+    (if (null he-expand-list)
+      (if old (he-reset-string))
+      (he-substitute-string (car he-expand-list))
+      (setq he-expand-list (cdr he-expand-list))
+      t)
+    ))
 
 ;; Done, now we just use it as a clause in our make-hippie-expand-function (as above)
 (setq hippie-expand-try-functions-list
