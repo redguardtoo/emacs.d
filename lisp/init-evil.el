a17051a8e42dff748dcf68f9532163d5b6d00a9b
Chen Bin <chenbin.sh@gmail.com>
2015-04-08 17:51:18 +1000

    upgrade git-timemacine & re-define its keybindings

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -242,16 +242,22 @@
   "ii" 'ido-imenu
   "ij" 'rimenu-jump
   "." 'evil-ex
-  ;; toggle overview,  @see http://emacs.wordpress.com/2007/01/16/quick-and-dirty-code-folding/
-  "gn" 'git-timemachine-show-next-revisio
+  ;; @see https://github.com/pidu/git-timemachine
+  ;; p: previous; n: next; w:hash; W:complete hash; g:nth version; q:quit
+  "gm" 'git-timemachine-toggle
+  "gn" 'git-timemachine-show-next-revision
   "gp" 'git-timemachine-show-previous-revision
   "gw" 'git-timemachine-kill-abbreviated-revision
+  "gW" 'git-timemachine-kill-revision
+  "gg" 'git-timemachine-show-nth-revision
+  "gq" 'git-timemachine-quit
+  ;; toggle overview,  @see http://emacs.wordpress.com/2007/01/16/quick-and-dirty-code-folding/
   "ov" '(lambda () (interactive) (set-selective-display (if selective-display nil 1)))
   "or" 'open-readme-in-git-root-directory
   "mq" '(lambda () (interactive) (man (concat "-k " (thing-at-point 'symbol))))
   "mgh" '(lambda () (interactive) (magit-show-commit "HEAD"))
-  "gg" '(lambda () (interactive) (w3m-search "g" (thing-at-point 'symbol)))
-  "qq" '(lambda () (interactive) (w3m-search "q" (thing-at-point 'symbol)))
+  "sg" '(lambda () (interactive) (w3m-search "g" (thing-at-point 'symbol)))
+  "sq" '(lambda () (interactive) (w3m-search "q" (thing-at-point 'symbol)))
   "gss" 'git-gutter:set-start-revision
   "gsh" '(lambda () (interactive) (git-gutter:set-start-revision "HEAD^")
            (message "git-gutter:set-start-revision HEAD^"))
