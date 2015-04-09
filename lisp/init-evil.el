d1b44c9df96ce805505ee748ce77cf31cb99fb7e
Chen Bin <chenbin.sh@gmail.com>
2015-04-09 18:37:15 +1000

    git-timemachine compatible with evil now

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -3,6 +3,14 @@
 (setq evil-symbol-word-search t)
 ;; load undo-tree and ert
 (add-to-list 'load-path "~/.emacs.d/site-lisp/evil/lib")
+
+;; @see https://bitbucket.org/lyro/evil/issue/511/let-certain-minor-modes-key-bindings
+(eval-after-load 'git-timemachine
+  '(progn
+     (evil-make-overriding-map git-timemachine-mode-map 'normal)
+     ;; force update evil keymaps after git-timemachine-mode loaded
+     (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps)))
+
 (require 'evil)
 
 (autoload 'dictionary-new-search "dictionary" "" t nil)
