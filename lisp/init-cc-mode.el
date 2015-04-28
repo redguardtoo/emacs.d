429d7f1ec1a9fca4dbbc8be61bef2f7c6477aa79
Chen Bin <chenbin.sh@gmail.com>
2015-04-28 11:44:58 +1000

    pick emacs default c style profile instead hack

--- lisp/init-cc-mode.el
+++ lisp/init-cc-mode.el
@@ -5,6 +5,8 @@
       'c-basic-offset
       (c-lineup-topmost-intro-cont langelem))))
 
+;; avoid default "gnu" style, use more popular one
+(setq c-default-style "linux")
 
 ;C/C++ SECTION
 (defun my-c-mode-hook ()
@@ -13,12 +15,11 @@
   ;; @see http://stackoverflow.com/questions/3509919/ \
   ;; emacs-c-opening-corresponding-header-file
   (local-set-key (kbd "C-x C-o") 'ff-find-other-file)
-  (local-set-key "\M-f" 'c-forward-into-nomenclature)
-  (local-set-key "\M-b" 'c-backward-into-nomenclature)
+
   (setq cc-search-directories '("." "/usr/include" "/usr/local/include/*" "../*/include" "$WXWIN/include"))
   (setq c-basic-offset 4)
-  (setq c-style-variables-are-local-p nil)
-  ;give me NO newline automatically after electric expressions are entered
+
+  ;; give me NO newline automatically after electric expressions are entered
   (setq c-auto-newline nil)
 
   ; @see http://xugx2007.blogspot.com.au/2007/06/benjamin-rutts-emacs-c-development-tips.html
@@ -36,46 +37,19 @@
               (message "NO COMPILATION ERRORS!")
               ))))
 
-  ;if (0)          becomes        if (0)
-  ;    {                          {
-  ;       ;                           ;
-  ;    }                          }
-  (c-set-offset 'substatement-open 0)
+  ;; wxWidgets setup
+  (c-set-offset 'topmost-intro-cont 'c-wx-lineup-topmost-intro-cont)
 
-  ;first arg of arglist to functions: tabbed in once
-  ;(default was c-lineup-arglist-intro-after-paren)
-  (c-set-offset 'arglist-intro '+)
-
-  ;second line of arglist to functions: tabbed in once
-  ;(default was c-lineup-arglist)
-  (c-set-offset 'arglist-cont-nonempty '+)
-
-  ;switch/case:  make each case line indent from switch
-  (c-set-offset 'case-label '+)
-
-  ;make the ENTER key indent next line properly
-  (local-set-key "\C-m" 'newline-and-indent)
-
-  ;syntax-highlight aggressively
-  ;(setq font-lock-support-mode 'lazy-lock-mode)
+  ;; syntax-highlight aggressively
+  ;; (setq font-lock-support-mode 'lazy-lock-mode)
   (setq lazy-lock-defer-contextually t)
   (setq lazy-lock-defer-time 0)
 
   ;make DEL take all previous whitespace with it
   (c-toggle-hungry-state 1)
 
-  ;make open-braces after a case: statement indent to 0 (default was '+)
-  (c-set-offset 'statement-case-open 0)
-
-  ;make a #define be left-aligned
+  ;; make a #define be left-aligned
   (setq c-electric-pound-behavior (quote (alignleft)))
-
-  ;wxwdigets stuff
-  (c-set-offset 'topmost-intro-cont 'c-wx-lineup-topmost-intro-cont)
-
-  ;do not impose restriction that all lines not top-level be indented at least
-  ;1 (was imposed by gnu style by default)
-  (setq c-label-minimum-indentation 0)
 
   (autoload 'c-turn-on-eldoc-mode "c-eldoc" "" t)
 
