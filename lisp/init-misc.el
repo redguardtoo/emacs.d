a144deee90c7faa6e93be75911bfb33846e82c27
Chen Bin <chenbin.sh@gmail.com>
2015-07-20 09:54:56 +1000

    require-final-newline should be nil

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -31,7 +31,7 @@
               set-mark-command-repeat-pop t
               tooltip-delay 1.5
               ;; void problems with crontabs, etc.
-              require-final-newline t
+              ;; require-final-newline t ; bad idea, could accidently edit others' code
               truncate-lines nil
               truncate-partial-width-windows nil
               ;; visible-bell has some issue
