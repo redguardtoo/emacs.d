2c90878cfd9aa9a2c20fab82e4c874d772445877
Chen Bin <chenbin.sh@gmail.com>
2015-08-12 16:10:45 +1000

    Revert "use evil-jumper"
    This reverts commit e9e88a972ca24c7db772ca73851f76da4f30b4b8.

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -521,14 +521,7 @@
                 (set-face-background 'mode-line (car color))
                 (set-face-foreground 'mode-line (cdr color))))))
 
-;; {{ commenter
 (require 'evil-nerd-commenter)
 (evilnc-default-hotkeys)
-;; }}
-
-;; {{ evil-jumper
-(require 'evil-jumper)
-(global-evil-jumper-mode)
-;; }}
 
 (provide 'init-evil)
