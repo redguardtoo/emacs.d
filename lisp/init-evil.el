31294b2fdb9b07ea058898dbd90d7aa72fc17bb6
Chen Bin <chenbin.sh@gmail.com>
2015-09-07 20:43:34 +1000

    find-file-in-project is frequently used

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -318,7 +318,7 @@
   "dj" 'dired-jump ;; open the dired from current file
   "ff" 'toggle-full-window ;; I use WIN+F in i3
   "ip" 'find-file-in-project
-  "is" 'find-file-in-project-by-selected
+  "kk" 'find-file-in-project-by-selected
   "tm" 'get-term
   "tff" 'toggle-frame-fullscreen
   "tfm" 'toggle-frame-maximized
