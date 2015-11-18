bfe57f3098b4b4dad4839b0781a1bd6070745b19
Chen Bin <chenbin.sh@gmail.com>
2015-11-18 17:59:58 +1100

    untabify

--- lisp/init-misc-lazy.el
+++ lisp/init-misc-lazy.el
@@ -503,7 +503,7 @@ Current position is preserved."
 Does not indent buffer, because it is used for a before-save-hook, and that
 might be bad."
   (interactive)
-  (untabify-buffer)
+  (untabify (point-min) (point-max))
   (delete-trailing-whitespace)
   (set-buffer-file-coding-system 'utf-8))
 
