f823176d4fdb7a605dd5864f5d3a6b7f42d6fc86
Chen Bin <chenbin.sh@gmail.com>
2015-12-03 16:20:20 +1100

    tweak find-file-in-project

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -67,6 +67,7 @@
 (autoload 'find-file-in-project "find-file-in-project" "" t)
 (autoload 'find-file-in-project-by-selected "find-file-in-project" "" t)
 (autoload 'ffip-get-project-root-directory "find-file-in-project" "" t)
+(setq ffip-match-path-instead-of-filename t)
 
 (defun neotree-project-dir ()
   "Open NeoTree using the git root."
