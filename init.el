cf90dadd6362a39b979583dd97ed0132dfd0d317
Chen Bin <chenbin.sh@gmail.com>
2016-02-02 13:44:51 +1100

    remove bookmark path

--- init.el
+++ init.el
@@ -147,7 +147,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "/home/cb/.emacs.bmk")
  '(git-gutter:handled-backends (quote (svn hg git)))
  '(safe-local-variable-values (quote ((lentic-init . lentic-orgel-org-init))))
  '(session-use-package t nil (session)))
