9c10640e203d4d97c357b6424727f0bafa946734
Chen Bin <chenbin.sh@gmail.com>
2016-03-06 22:57:42 +1100

    clean init-elpa.el

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -1,75 +1,8 @@
 (require 'package)
 
 ;;------------------------------------------------------------------------------
-;; Patch up annoying package.el quirks
+;; Cutomized setup, you can tweak repository and package freely
 ;;------------------------------------------------------------------------------
-(defadvice package-generate-autoloads (after close-autoloads (name pkg-dir) activate)
-  "Stop package.el from leaving open autoload files lying around."
-  (let ((path (expand-file-name (concat
-                                 ;; name is string when emacs <= 24.3.1,
-                                 (if (symbolp name) (symbol-name name) name)
-                                 "-autoloads.el") pkg-dir)))
-    (with-current-buffer (find-file-existing path)
-      (kill-buffer nil))))
-
-;;------------------------------------------------------------------------------
-;; Add support to package.el for pre-filtering available packages
-;;------------------------------------------------------------------------------
-(defvar package-filter-function nil
-  "Optional predicate function used to internally filter packages used by package.el.
-
-The function is called with the arguments PACKAGE VERSION ARCHIVE, where
-PACKAGE is a symbol, VERSION is a vector as produced by `version-to-list', and
-ARCHIVE is the string name of the package archive.")
-
-(defadvice package--add-to-archive-contents
-  (around filter-packages (package archive) activate)
-  "Add filtering of available packages using `package-filter-function', if non-nil."
-  (when (or (null package-filter-function)
-      (funcall package-filter-function
-         (car package)
-         (funcall (if (fboundp 'package-desc-version)
-          'package--ac-desc-version
-        'package-desc-vers)
-            (cdr package))
-         archive))
-    ad-do-it))
-
-;;------------------------------------------------------------------------------
-;; On-demand installation of packages
-;;------------------------------------------------------------------------------
-(defun require-package (package &optional min-version no-refresh)
-  "Ask elpa to install given PACKAGE."
-  (if (package-installed-p package min-version)
-      t
-    (if (or (assoc package package-archive-contents) no-refresh)
-        (package-install package)
-      (progn
-        (package-refresh-contents)
-        (require-package package min-version t)))))
-
-
-;;------------------------------------------------------------------------------
-;; Standard package repositories
-;;------------------------------------------------------------------------------
-
-;; We include the org repository for completeness, but don't use it.
-;; Lock org-mode temporarily:
-;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-
-(setq package-archives '(("melpa" . "http://melpa.org/packages/")
-                         ("melpa-stable" . "http://stable.melpa.org/packages/")
-                         ;; uncomment below line if you need use GNU ELPA
-                         ;; ("gnu" . "http://elpa.gnu.org/packages/")
-                         ))
-
-;; Un-comment below line if you download zip file
-;; from https://github.com/redguardtoo/myelpa/archive/master.zip
-;; and extract its content into ~/myelpa/
-;; (setq package-archives '(("myelpa" . "~/myelpa")))
-
-;; Or Un-comment below line if you prefer installing package from https://github.com/redguardtoo/myelpa/ directly
-;; (setq package-archives '(("myelpa" . "https://raw.github.com/redguardtoo/myelpa/master/")))
 
 ;; List of VISIBLE packages from melpa-unstable (http://melpa.org)
 ;; Feel free to add more packages!
@@ -171,6 +104,69 @@ ARCHIVE is the string name of the package archive.")
     hemisu-theme
     heroku-theme)
   "Don't install any Melpa packages except these packages")
+
+;; We include the org repository for completeness, but don't use it.
+;; Lock org-mode temporarily:
+;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")
+                         ;; uncomment below line if you need use GNU ELPA
+                         ;; ("gnu" . "http://elpa.gnu.org/packages/")
+                         ))
+
+;; Un-comment below line if your extract https://github.com/redguardtoo/myelpa/archive/master.zip into ~/myelpa/
+;; (setq package-archives '(("myelpa" . "~/myelpa")))
+
+;; Or Un-comment below line if you install package from https://github.com/redguardtoo/myelpa/
+;; (setq package-archives '(("myelpa" . "https://raw.github.com/redguardtoo/myelpa/master/")))
+
+
+
+;;------------------------------------------------------------------------------
+;; Internal implementation, newbies should NOT touch code below this line!
+;;------------------------------------------------------------------------------
+
+;; Patch up annoying package.el quirks
+(defadvice package-generate-autoloads (after close-autoloads (name pkg-dir) activate)
+  "Stop package.el from leaving open autoload files lying around."
+  (let ((path (expand-file-name (concat
+                                 ;; name is string when emacs <= 24.3.1,
+                                 (if (symbolp name) (symbol-name name) name)
+                                 "-autoloads.el") pkg-dir)))
+    (with-current-buffer (find-file-existing path)
+      (kill-buffer nil))))
+
+;; Add support to package.el for pre-filtering available packages
+(defvar package-filter-function nil
+  "Optional predicate function used to internally filter packages used by package.el.
+
+The function is called with the arguments PACKAGE VERSION ARCHIVE, where
+PACKAGE is a symbol, VERSION is a vector as produced by `version-to-list', and
+ARCHIVE is the string name of the package archive.")
+
+(defadvice package--add-to-archive-contents
+  (around filter-packages (package archive) activate)
+  "Add filtering of available packages using `package-filter-function', if non-nil."
+  (when (or (null package-filter-function)
+      (funcall package-filter-function
+         (car package)
+         (funcall (if (fboundp 'package-desc-version)
+          'package--ac-desc-version
+        'package-desc-vers)
+            (cdr package))
+         archive))
+    ad-do-it))
+
+;; On-demand installation of packages
+(defun require-package (package &optional min-version no-refresh)
+  "Ask elpa to install given PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
 
 ;; Don't take Melpa versions of certain packages
 (setq package-filter-function
