c487fd7e5ba9c86cecff2c29dc4abf4887910f79
Chen Bin <chenbin.sh@gmail.com>
2016-03-06 13:13:11 +1100

    quick startup

--- init.el
+++ init.el
@@ -22,124 +22,132 @@
                    (*linux* nil)
                    (t nil)))
 
-(require 'init-modeline)
-(require 'cl-lib)
-(require 'init-compat)
-(require 'init-utils)
-(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+;; @see https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+;; Normally file-name-handler-alist is set to
+;; (("\\`/[^/]*\\'" . tramp-completion-file-name-handler)
+;; ("\\`/[^/|:][^/|]*:" . tramp-file-name-handler)
+;; ("\\`/:" . file-name-non-special))
+;; Which means on every .el and .elc file loaded during start up, it has to runs those regexps against the filename.
+(let ((file-name-handler-alist nil))
+  (require 'init-modeline)
+  (require 'cl-lib)
+  (require 'init-compat)
+  (require 'init-utils)
+  (require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
 
-;; Windows configuration, assuming that cygwin is installed at "c:/cygwin"
-;; (condition-case nil
-;;     (when *win64*
-;;       ;; (setq cygwin-mount-cygwin-bin-directory "c:/cygwin/bin")
-;;       (setq cygwin-mount-cygwin-bin-directory "c:/cygwin64/bin")
-;;       (require 'setup-cygwin)
-;;       ;; better to set HOME env in GUI
-;;       ;; (setenv "HOME" "c:/cygwin/home/someuser")
-;;       )
-;;   (error
-;;    (message "setup-cygwin failed, continue anyway")
-;;    ))
+  ;; Windows configuration, assuming that cygwin is installed at "c:/cygwin"
+  ;; (condition-case nil
+  ;;     (when *win64*
+  ;;       ;; (setq cygwin-mount-cygwin-bin-directory "c:/cygwin/bin")
+  ;;       (setq cygwin-mount-cygwin-bin-directory "c:/cygwin64/bin")
+  ;;       (require 'setup-cygwin)
+  ;;       ;; better to set HOME env in GUI
+  ;;       ;; (setenv "HOME" "c:/cygwin/home/someuser")
+  ;;       )
+  ;;   (error
+  ;;    (message "setup-cygwin failed, continue anyway")
+  ;;    ))
 
-(require 'idle-require)
-(require 'init-elpa)
-(require 'init-exec-path) ;; Set up $PATH
-(require 'init-frame-hooks)
-;; any file use flyspell should be initialized after init-spelling.el
-;; actually, I don't know which major-mode use flyspell.
-(require 'init-spelling)
-(require 'init-xterm)
-(require 'init-gui-frames)
-(require 'init-ido)
-(require 'init-dired)
-(require 'init-uniquify)
-(require 'init-ibuffer)
-(require 'init-flymake)
-(require 'init-smex)
-(require 'init-ivy)
-(require 'init-hippie-expand)
-(require 'init-windows)
-(require 'init-sessions)
-(require 'init-git)
-(require 'init-crontab)
-(require 'init-markdown)
-(require 'init-erlang)
-(require 'init-javascript)
-(require 'init-org)
-(require 'init-org-mime)
-(require 'init-css)
-(require 'init-python-mode)
-(require 'init-haskell)
-(require 'init-ruby-mode)
-(require 'init-lisp)
-(require 'init-elisp)
-(require 'init-yasnippet)
-;; Use bookmark instead
-(require 'init-zencoding-mode)
-(require 'init-cc-mode)
-(require 'init-gud)
-(require 'init-linum-mode)
-;; (require 'init-gist)
-(require 'init-moz)
-(require 'init-gtags)
-;; use evil mode (vi key binding)
-(require 'init-evil)
-(require 'init-sh)
-(require 'init-ctags)
-(require 'init-bbdb)
-(require 'init-gnus)
-(require 'init-lua-mode)
-(require 'init-workgroups2)
-(require 'init-term-mode)
-(require 'init-web-mode)
-(require 'init-slime)
-(require 'init-clipboard)
-(require 'init-company)
-(require 'init-chinese-pyim) ;; cannot be idle-required
-;; need statistics of keyfreq asap
-(require 'init-keyfreq)
-(require 'init-httpd)
+  (require 'idle-require)
+  (require 'init-elpa)
+  (require 'init-exec-path) ;; Set up $PATH
+  (require 'init-frame-hooks)
+  ;; any file use flyspell should be initialized after init-spelling.el
+  ;; actually, I don't know which major-mode use flyspell.
+  (require 'init-spelling)
+  (require 'init-xterm)
+  (require 'init-gui-frames)
+  (require 'init-ido)
+  (require 'init-dired)
+  (require 'init-uniquify)
+  (require 'init-ibuffer)
+  (require 'init-flymake)
+  (require 'init-smex)
+  (require 'init-ivy)
+  (require 'init-hippie-expand)
+  (require 'init-windows)
+  (require 'init-sessions)
+  (require 'init-git)
+  (require 'init-crontab)
+  (require 'init-markdown)
+  (require 'init-erlang)
+  (require 'init-javascript)
+  (require 'init-org)
+  (require 'init-org-mime)
+  (require 'init-css)
+  (require 'init-python-mode)
+  (require 'init-haskell)
+  (require 'init-ruby-mode)
+  (require 'init-lisp)
+  (require 'init-elisp)
+  (require 'init-yasnippet)
+  ;; Use bookmark instead
+  (require 'init-zencoding-mode)
+  (require 'init-cc-mode)
+  (require 'init-gud)
+  (require 'init-linum-mode)
+  ;; (require 'init-gist)
+  (require 'init-moz)
+  (require 'init-gtags)
+  ;; use evil mode (vi key binding)
+  (require 'init-evil)
+  (require 'init-sh)
+  (require 'init-ctags)
+  (require 'init-bbdb)
+  (require 'init-gnus)
+  (require 'init-lua-mode)
+  (require 'init-workgroups2)
+  (require 'init-term-mode)
+  (require 'init-web-mode)
+  (require 'init-slime)
+  (require 'init-clipboard)
+  (require 'init-company)
+  (require 'init-chinese-pyim) ;; cannot be idle-required
+  ;; need statistics of keyfreq asap
+  (require 'init-keyfreq)
+  (require 'init-httpd)
 
-;; projectile costs 7% startup time
+  ;; projectile costs 7% startup time
 
-;; misc has some crucial tools I need immediately
-(require 'init-misc)
+  ;; misc has some crucial tools I need immediately
+  (require 'init-misc)
 
-;; comment below line if you want to setup color theme in your own way
-(if (or (display-graphic-p) (string-match-p "256color"(getenv "TERM"))) (require 'init-color-theme))
+  ;; comment below line if you want to setup color theme in your own way
+  (if (or (display-graphic-p) (string-match-p "256color"(getenv "TERM"))) (require 'init-color-theme))
 
-(require 'init-emacs-w3m)
-(require 'init-hydra)
+  (require 'init-emacs-w3m)
+  (require 'init-hydra)
 
-;; {{ idle require other stuff
-(setq idle-require-idle-delay 3)
-(setq idle-require-symbols '(init-misc-lazy
-                             init-which-func
-                             init-fonts
-                             init-hs-minor-mode
-                             init-textile
-                             init-csv
-                             init-writting
-                             init-doxygen
-                             init-pomodoro
-                             init-emacspeak
-                             init-artbollocks-mode
-                             init-semantic))
-(idle-require-mode 1) ;; starts loading
-;; }}
+  ;; {{ idle require other stuff
+  (setq idle-require-idle-delay 3)
+  (setq idle-require-symbols '(init-misc-lazy
+                               init-which-func
+                               init-fonts
+                               init-hs-minor-mode
+                               init-textile
+                               init-csv
+                               init-writting
+                               init-doxygen
+                               init-pomodoro
+                               init-emacspeak
+                               init-artbollocks-mode
+                               init-semantic))
+  (idle-require-mode 1) ;; starts loading
+  ;; }}
 
-(when (require 'time-date nil t)
-   (message "Emacs startup time: %d seconds."
-    (time-to-seconds (time-since emacs-load-start-time))))
+  (when (require 'time-date nil t)
+    (message "Emacs startup time: %d seconds."
+             (time-to-seconds (time-since emacs-load-start-time))))
 
-;;----------------------------------------------------------------------------
-;; Locales (setting them earlier in this file doesn't work in X)
-;;----------------------------------------------------------------------------
-(require 'init-locales)
+  ;;----------------------------------------------------------------------------
+  ;; Locales (setting them earlier in this file doesn't work in X)
+  ;;----------------------------------------------------------------------------
+  (require 'init-locales)
 
-;; my personal setup, other major-mode specific setup need it.
-;; It's dependent on init-site-lisp.el
-(if (file-exists-p "~/.custom.el") (load-file "~/.custom.el"))
+  ;; my personal setup, other major-mode specific setup need it.
+  ;; It's dependent on init-site-lisp.el
+  (if (file-exists-p "~/.custom.el") (load-file "~/.custom.el"))
+  )
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
