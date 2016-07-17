728e470eed5390ac31f09a880afdd7a4f4e8f0b9
Chen Bin <chenbin.sh@gmail.com>
2016-07-17 22:24:40 +1000

    upgrade gmail2bbdb

--- site-lisp/gmail2bbdb/gmail2bbdb.el
+++ site-lisp/gmail2bbdb/gmail2bbdb.el
@@ -1,39 +1,63 @@
-;;; gmail2bbdb.el --- import email and name into bbdb from vcard
+;;; gmail2bbdb.el --- import email and name into bbdb from vcard.
 
-;; Copyright (C) 2014 Chen Bin
+;; Copyright (C) 2014, 2015 Chen Bin
 ;; Author: Chen Bin <chenbin.sh@gmail.com>
 ;; URL: http://github.com/redguardtoo/gmail2bbdb
 ;; Keywords: vcard bbdb email contact gmail
-;; Version: 0.0.2
+;; Version: 0.0.4
 
 ;; This file is not part of GNU Emacs.
 
-;; This file is free software (GPLv3 License)
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
 
-;; How to set it up:
-;; See README.org which is distributed with this file
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program ; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; To setup, insert below code into ~/.emacs:
+;;   (add-to-list 'load-path "~/.emacs.d/lisp/")
+;;   (autoload 'gmail2bbdb-import-file "gmail2bbdb" nil t nil)
+;;
+;; Usage:
+;;   - At https://www.google.com/contacts, click "More->Export->vCard format->Export".
+;;     The file "contacts.vcf" will be downloaded.
+;;   - Run command "M-x gmail2bbdb-import-file" and select contacts.vcf.
+;;     ~/.bbdb will be created.
+;;
 
 ;;; Code:
 
-(defvar gmail2bbdb-bbdb-file "~/.bbdb.el"
-  "the full path of exported BBDB file")
+(defvar gmail2bbdb-bbdb-file "~/.bbdb"
+  "The full path of exported BBDB file.")
+
 (defvar gmail2bbdb-excluded-email-regex-list '("^noreply.*"
                                                "notify.*@disqus.net"
                                                ".*@noreply.github.com$"
                                                "reply.*@reply.github.com"
                                                "do-not-reply@stackoverflow.com")
-  "email matching any regex in this list will NOT be exported")
+  "Email matching any regex from the list will NOT be exported")
 
 (defun gmail2bbdb--is-valid-email (eml)
-  (let ((i 0) found re)
-    (while (and (not found)
+  (let ((i 0) excluded re)
+    (while (and (not excluded)
                 (< i (length gmail2bbdb-excluded-email-regex-list)))
       (setq re (nth i gmail2bbdb-excluded-email-regex-list))
       (if (string-match re eml)
-        (setq found t))
+        (setq excluded t))
       (setq i (1+ i))
       )
-    (not found)))
+    (not excluded)))
 
 ;; ["Spolsky" "Joel" nil ("Spolsky Joel") nil nil nil ("spolsky@fogcreek.com") nil nil]
 (defun gmail2bbdb--extract-item (str)
@@ -89,7 +113,7 @@ records will be REMOVED."
           (insert ";; -*- mode: Emacs-Lisp; coding: utf-8; -*-\n")
           (insert ";;; file-format: 7\n")
           (insert rlt)
-          (write-file gmail2bbdb-bbdb-file))
+          (write-file (file-truename gmail2bbdb-bbdb-file)))
       (message "No email found"))
     ))
 
