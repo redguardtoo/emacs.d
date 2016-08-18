df250d0d7f5976dc4b14fbd0c0405e2a895c992f
Chen Bin <chenbin.sh@gmail.com>
2016-08-19 09:24:55 +1000

    elpy changed its API

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -5,7 +5,7 @@
   (unless (is-buffer-file-temp)
     ;; run command `pip install jedi flake8 importmagic` in shell,
     ;; or just check https://github.com/jorgenschaefer/elpy
-    (elpy-mode 1)
+    (if (fboundp 'elpy-enable) (elpy-enable) (elpy-mode 1))
     ;; http://emacs.stackexchange.com/questions/3322/python-auto-indent-problem/3338#3338
     ;; emacs 24.4 only
     (setq electric-indent-chars (delq ?: electric-indent-chars))))
