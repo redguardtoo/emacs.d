252de90f394a174a10f1e9fbaf2d35f64e2b18d0
Chen Bin <chenbin.sh@gmail.com>
2016-12-07 20:45:15 +1100

    no menu bar

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -1,26 +1,20 @@
-;;----------------------------------------------------------------------------
 ;; Suppress GUI features
-;;----------------------------------------------------------------------------
 (setq use-file-dialog nil)
 (setq use-dialog-box nil)
 (setq inhibit-startup-screen t)
 (setq inhibit-startup-echo-area-message t)
 
-;;----------------------------------------------------------------------------
 ;; Show a marker in the left fringe for lines not in the buffer
-;;----------------------------------------------------------------------------
 (setq indicate-empty-lines t)
 
-;; NO tool bar or scroll bar
-(when (fboundp 'tool-bar-mode)
+;; NO tool bar
+(if (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
-(when (fboundp 'set-scroll-bar-mode)
+;; no scroll bar
+(if (fboundp 'set-scroll-bar-mode)
   (set-scroll-bar-mode nil))
-
-(add-hook 'after-make-frame-functions
-          (lambda (frame)
-            (with-selected-frame frame
-              (unless window-system
-                (set-frame-parameter nil 'menu-bar-lines 0)))))
+;; no menu bar
+(if (fboundp 'menu-bar-mode)
+  (menu-bar-mode -1))
 
 (provide 'init-gui-frames)
