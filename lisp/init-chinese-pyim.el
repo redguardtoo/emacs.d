a71893f4ea14626df00b968b7266250a86958a29
Chen Bin <chenbin.sh@gmail.com>
2017-02-27 20:53:21 +1100

    better chinese-pyim setup

--- lisp/init-chinese-pyim.el
+++ lisp/init-chinese-pyim.el
@@ -30,10 +30,58 @@
 (global-set-key (kbd "C-\\") 'evil-toggle-input-method)
 ;; }}
 
+(defvar my-pyim-directory
+  "~/.eim")
+
+(add-to-list 'auto-mode-alist '("\\.pyim\\'" . text-mode))
+
+(defun my-pyim-personal-dict (&optional dict-name)
+  (file-truename (concat (file-name-as-directory my-pyim-directory)
+                         (or dict-name "personal.pyim"))))
+
+(defun my-pyim-export-dictionary ()
+  (interactive)
+  (with-temp-buffer
+    (maphash
+     #'(lambda (key value)
+         ;; only export two character word
+         (if (string-match "-" key)
+             (insert (concat key
+                             " "
+                             (mapconcat #'identity value ""))
+                     "\n")))
+     pyim-dcache-icode2word)
+    (unless (and my-pyim-directory
+                 (file-directory-p my-pyim-directory))
+      (setq my-pyim-directory
+            (read-directory-name "Personal Chinese dictionary directory:")))
+    (if my-pyim-directory
+        (write-file (my-pyim-personal-dict)))))
+
 (eval-after-load 'chinese-pyim
   '(progn
-     (setq pyim-punctuation-translate-p nil) ;; use western punctuation (ban jiao fu hao)
+     ;; I'm OK with a smaller dictionary
+     (chinese-pyim-basedict-enable)
+     ;; use western punctuation (ban jiao fu hao)
+     (setq pyim-punctuation-dict nil)
+     ;; always input English when isearch
+     (setq pyim-isearch-enable-pinyin-search t)
      (setq default-input-method "chinese-pyim")
+     ;; re-order backends, I prefer less typing
+     (setq pyim-backends '(pinyin-shortcode
+                           pinyin-znabc
+                           dcache-personal
+                           dcache-common
+                           pinyin-chars))
+     ;; use personal dictionary
+     (if (and my-pyim-directory
+              (file-exists-p (my-pyim-personal-dict)))
+         (add-to-list 'pyim-dicts (list :name "personal" :file (my-pyim-personal-dict))))
+
+     ;; You can also set up the great dictionary (80M) the same way as peronsal dictionary
+     ;; great dictionary can be downloaded this way:
+     ;; `curl -L https://github.com/tumashu/chinese-pyim-greatdict/raw/master/pyim-greatdict.pyim.gz | zcat > ~/.eim/pyim-greatdict.pyim`
+
      ;; don't use tooltip
      (setq pyim-use-tooltip 'popup)))
 
