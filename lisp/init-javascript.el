326bf98a9056a222ab9c4cf0023e66dd4758b6be
Chen Bin <chenbin.sh@gmail.com>
2017-03-23 13:26:58 +1100

    work around jsx indent issue

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -338,6 +338,18 @@ INDENT-SIZE decide the indentation level.
   (js-send-buffer))
 ;; }}
 
+(defadvice js-jsx-indent-line (after js-jsx-indent-line-after-hack activate)
+  "Workaround sgml-mode and follow airbnb component style."
+  (let* ((cur-line (buffer-substring-no-properties
+                    (line-beginning-position)
+                    (line-end-position))))
+    (if (string-match "^\\( +\\)\/?> *$" cur-line)
+      (let* ((empty-spaces (match-string 1 cur-line)))
+        (replace-regexp empty-spaces
+                        (make-string (- (length empty-spaces) sgml-basic-offset) 32)
+                        nil
+                        (line-beginning-position) (line-end-position))))))
+
 (setq-default js2-additional-externs
               '("$"
                 "$A" ; salesforce lightning component
