9e24c8379db6c351c9d2a2a8f636471f78c78532
Chen Bin <chenbin.sh@gmail.com>
2017-03-24 20:50:36 +1100

    optimized emacs startup time

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -3,8 +3,8 @@
      (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"))
        (add-to-list 'exec-path-from-shell-variables var))))
 
-
-(when (memq window-system '(mac ns))
+(when (and window-system
+           (memq window-system '(mac ns)))
   (exec-path-from-shell-initialize))
 
 (provide 'init-exec-path)
