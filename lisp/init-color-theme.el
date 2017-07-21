cd503142bc008b2c611ac884e50f074932323baf
Chen Bin <chenbin.sh@gmail.com>
2017-07-21 10:24:58 +1000

    update color theme documentation

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -17,21 +17,24 @@
 Fallback theme is used only if the console does NOT support 256 colors."
   (interactive)
   (cond
-   ((string= my-current-color-theme "molokai")
+   ((string= my-current-color-theme "favorite")
     ;; fallback color theme from color-theme library
     (unless color-theme-initialized (color-theme-initialize))
+    ;; {{ fallback built in color theme
     (color-theme-deep-blue)
+    ;; }}
     (setq my-current-color-theme "fallback"))
    (t
-    ;; major color theme we use
+    ;; {{ enable my favourite color theme
     (unless (featurep 'color-theme-molokai)
       (require 'color-theme-molokai))
     (color-theme-molokai)
-    (setq my-current-color-theme "molokai"))))
+    ;; }}
+    (setq my-current-color-theme "favorite"))))
 ;; turn on the color theme now!
 (my-toggle-color-theme)
 
-;; This line must be after color-theme-molokai! Don't know why.
+;; This line must be after color theme setup! Don't know why.
 (setq color-theme-illegal-faces "^\\(w3-\\|dropdown-\\|info-\\|linum\\|yas-\\|font-lock\\|dired-directory\\)")
 
 (provide 'init-color-theme)
