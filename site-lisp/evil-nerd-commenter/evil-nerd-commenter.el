b284bf0b4bd2d8e66057d609d90fb45e4dcaf1f9
Chen Bin <chenbin.sh@gmail.com>
2017-08-15 12:41:45 +1000

    upgrade evil-nerd-commenter

--- site-lisp/evil-nerd-commenter/evil-nerd-commenter.el
+++ site-lisp/evil-nerd-commenter/evil-nerd-commenter.el
@@ -704,9 +704,11 @@ If NO-EVIL-KEYBINDINGS is t, we don't define keybindings in evil-mode."
     (eval-after-load 'evil
       '(progn
          (define-key evil-normal-state-map ",ci" 'evilnc-comment-or-uncomment-lines)
+         (define-key evil-visual-state-map ",ci" 'evilnc-comment-or-uncomment-lines)
          (define-key evil-normal-state-map ",cl" 'evilnc-quick-comment-or-uncomment-to-the-line)
          (define-key evil-normal-state-map ",ll" 'evilnc-quick-comment-or-uncomment-to-the-line)
          (define-key evil-normal-state-map ",cc" 'evilnc-copy-and-comment-lines)
+         (define-key evil-visual-state-map ",cc" 'evilnc-copy-and-comment-lines)
          (define-key evil-normal-state-map ",cp" 'evilnc-comment-or-uncomment-paragraphs)
          (define-key evil-normal-state-map ",cr" 'comment-or-uncomment-region)
          (define-key evil-normal-state-map ",cv" 'evilnc-toggle-invert-comment-line-by-line)))
