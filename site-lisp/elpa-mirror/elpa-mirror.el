68c4b7e740cd331f8d7c98afa88fa5d98be3c0a3
Chen Bin <chenbin.sh@gmail.com>
2018-01-14 18:30:00 +1100

    upgrade elpa-mirror

--- site-lisp/elpa-mirror/elpa-mirror.el
+++ site-lisp/elpa-mirror/elpa-mirror.el
@@ -1,10 +1,11 @@
-;;; elpa-mirror.el --- Create local package repository
+;;; elpa-mirror.el --- Create local package repository so package upgrade never breaks
 
-;; Copyright (C) 2014 Chen Bin
+;; Copyright (C) 2014-2017 Chen Bin
 
 ;; Author: Chen Bin <chenbin.sh@gmail.com>
 ;; URL: http://github.com/redguardtoo/elpa-mirror
-;; Version: 2.1.0
+;; Package-Requires: ((emacs "24.4"))
+;; Version: 2.1.1
 ;; Keywords: cloud mirror elpa
 ;;
 ;; This file is not part of GNU Emacs.
@@ -33,7 +34,7 @@
 ;;  If you use native Windows Emacs, install Cygwin or MSYS2.
 ;;
 ;; Usage in Shell,
-;;   emacs --batch -l ~/.emacs.d/init.el
+;;   Emacs --batch -l ~/.emacs.d/init.el
 ;;         -l ~/any-directory-you-prefer/elpa-mirror.el \
 ;;         --eval='(setq elpamr-default-output-directory "~/myelpa")' \
 ;;         --eval='(elpamr-create-mirror-for-installed)
@@ -48,84 +49,54 @@
 ;;; Code:
 (require 'package)
 
-(defvar elpamr-default-output-directory
-  nil
+(defcustom elpamr-default-output-directory nil
   "The output directory.
-If nil, you need provide one when `elpamr-create-mirror-for-installed'.")
+If nil, you need provide one when `elpamr-create-mirror-for-installed'."
+  :type '(choice directory (const :tags "None" nil))
+  :group 'elpa-mirror)
 
-(defvar elpamr-exclude-package-from-repositories
-  '("myelpa")
-  "Exclude packages from certain repositories.")
+(defcustom elpamr-exclude-packages nil
+  "Nameos of excluded packages"
+  :type '(repeat string)
+  :group 'elpa-mirror)
+
+(defcustom elpamr-finished-hook nil
+  "Hook run when command `elpamr-create-mirror-for-installed' run finished.
+The hook function have one argument: output-directory."
+  :group 'elpa-mirror
+  :type 'hook)
 
 (defvar elpamr-debug nil "Show debug message.")
 
-(defun elpamr--get-info-array (item)
-  (if (elpamr--is-new-package)
-      (cadr item)
-    (cdr item)))
+(defun elpamr--package-desc (item)
+  "Extract package information from ITEM."
+  (cadr item))
 
 (defun elpamr--is-mac ()
+  "Is macOS?"
   (eq system-type 'darwin))
 
 (defun elpamr--create-one-item-for-archive-contents (pkg)
-  "We can use package-alist directly.
-This API will append some meta info into package-alist."
-  (let* ((name (car pkg))
-         item
-         package-content
-         repo
-         found
-         (i 0))
-
-    ;; package-archive-contents is the list of ALL packages
-    (while (and (not found)
-                (< i (length package-archive-contents)))
-      (setq package-content (nth i package-archive-contents))
-      ;; well, all we need do is to write the actual version into package-content
-
-      (when (equal name (car package-content))
-        ;; real version used instead the one in archive-contents
-        (if (arrayp (elpamr--get-info-array package-content))
-            (elpamr--set-version
-             package-content
-             (elpamr--get-version pkg)))
-
-        (setq item package-content)
-        (setq found t)
-        )
-      (setq i (1+ i)))
-
-    (unless found
-      ;; make do with installed package, looks it's deleted in archive-contents
-      (setq item pkg))
-
-    (setq repo (elt (cdr package-content) 4))
-    (if (listp repo)  (setq repo (elt (cdr package-content) 5)))
-    (if (member repo elpamr-exclude-package-from-repositories)
-        (setq item nil))
-
-    item))
-
+  "Access PKG extracted from `package-alist' directly."
+  (unless (member (symbol-name (car pkg))elpamr-exclude-packages)
+    pkg))
 
 (defun elpamr--extract-info-from-dir (dirname)
-  "Return `(list package-name integer-version-number)' or nil."
+  "Extract information from DIRNAME.
+Return `(list package-name integer-version-number)' or nil."
   (interactive)
   (if (string-match "\\(.*\\)-\\([0-9.]+\\)$" dirname)
       ;; (list name version)
       (list (match-string 1 dirname)
             (split-string (match-string 2 dirname) "\\."))))
 
-(defun elpamr--is-new-package ()
-  "Emacs 24+ has different structure from 23."
-  (or (and (>= emacs-major-version 24)
-           (>= emacs-minor-version 4))
-      (>= emacs-major-version 25)))
-
 (defun elpamr--win-executable-find (driver path exe)
+  "GNU Find executable with DRIVER/PATH/EXE information provided."
   (if (executable-find (concat driver path exe))
       (concat driver path exe)))
 
 (defun elpamr--executable-find (exe)
+  "GNU Find EXE on Windows."
   (or (and (eq system-type 'windows-nt)
            (or
             ;; cygwin
@@ -148,7 +119,8 @@ This API will append some meta info into package-alist."
       exe))
 
 (defun elpamr--fullpath (parent file &optional no-convertion)
-  "Full path of 'parent/file'."
+  "Full path of 'PARENT/FILE'.
+If NO-CONVERTION is t,  it's UNIX path."
   (let* ((rlt (file-truename (concat (file-name-as-directory parent) file))))
     (if (and (eq system-type 'windows-nt) (not no-convertion))
         (let* ((cyg-cmd (format "%s -u \"%s\""
@@ -160,98 +132,52 @@ This API will append some meta info into package-alist."
     (if elpamr-debug (message "elpamr--fullpath called => %s" rlt))
     rlt))
 
-(defun elpamr--input-fullpath (file)
-  (elpamr--fullpath package-user-dir file))
-
-(defun elpamr--output-fullpath (file &optional no-convertion)
-  "Return full path of output file, given the FILE."
-  (elpamr--fullpath elpamr-default-output-directory file no-convertion))
-
-(defun elpamr--clean-package-description (descr)
+(defun elpamr--clean-package-description (description)
+  "Clean DESCRIPTION."
   (replace-regexp-in-string "-\*-.*-\*-" ""
-                            (replace-regexp-in-string "\"" "" descr t)
+                            (replace-regexp-in-string "\"" "" description t)
                             t))
 
-(defun elpamr--set-version (item version)
-  (let* ((a (elpamr--get-info-array item)))
-    (if (elpamr--is-new-package)
-        (aset a 2 version)
-      (aset a 0 version))))
-
 (defun elpamr--get-dependency (item)
-  (let* ((a (elpamr--get-info-array item)))
-    (if (elpamr--is-new-package)
-        (elt a 4)
-      (elt a 1))))
+  "Get ITEM's dependency."
+  (package-desc-reqs (elpamr--package-desc item)))
 
 (defun elpamr--get-version (item)
-  (let* ((a (elpamr--get-info-array item)))
-    (if (elpamr--is-new-package)
-        (elt a 2)
-      (elt a 0))))
+  "Get ITEM's version."
+  (package-desc-version (elpamr--package-desc item)))
 
-(defun elpamr--get-repo (item)
-  (let* ((a (elpamr--get-info-array item)))
-    (if (elpamr--is-new-package)
-        (if (> (length a) 6)
-            (elt a 6) "legacy")
-      (if (> (length a) 4)
-          (elt a 4) "legacy"))))
-
-(defun elpamr--get-type (item)
-  (let* ((a (elpamr--get-info-array item))
-         (rlt (if (elpamr--is-new-package)
-                  (if (> (length a) 5)
-                      (elt a 5) 'tar)
-                (if (> (length a) 3)
-                    (elt a 3) 'tar))))
-    (if (not rlt) (setq rlt 'tar))
-    rlt))
-
-(defun elpamr--create-complete-package-name (item)
-  (concat (symbol-name (car item))
-          "-"
-          (mapconcat (lambda (arg)
-                       (format "%d" arg))
-                     (elpamr--get-version item)  ".")))
-
-(defun elpamr--is-single-el (item)
-  (equal 'single (elpamr--get-type item)))
-
-(defun elpamr--get-description (item)
-  (let* ((a (elpamr--get-info-array item)) )
-    (if (elpamr--is-new-package)
-        (elt a 3)
-      (elt a 2))))
-
-(defun elpamr--is-single-el-by-name (name pkglist)
-  (let* (rlt)
-    (dolist (pkg pkglist)
-      (if (string= (car pkg) name)
-          (setq rlt (elpamr--is-single-el pkg))))
-    rlt))
+(defun elpamr--get-summary (item)
+  "Get ITEM's description."
+  (package-desc-summary (elpamr--package-desc item)))
 
 (defun elpamr--one-item-for-archive-contents (final-pkg)
-  (let* ((a (elpamr--get-info-array final-pkg)))
-    (format " (%s . [%S %S \"%s\" %S])\n"
+  "Format FINAL-PKG's information into a string for archive-contents."
+  (let* ((a (elpamr--package-desc final-pkg)))
+    (format " (%s . [%S %S \"%s\" tar])\n"
             (car final-pkg)
             (elpamr--get-version final-pkg)
             (elpamr--get-dependency final-pkg)
-            (elpamr--clean-package-description (elpamr--get-description final-pkg))
-            (elpamr--get-type final-pkg))))
+            (elpamr--clean-package-description (elpamr--get-summary final-pkg)))))
 
 ;;;###autoload
 (defun elpamr-version ()
   "Current version."
   (interactive)
-  (message "2.1.0"))
+  (message "2.1.1"))
 
 ;;;###autoload
-(defun elpamr-create-mirror-for-installed ()
+(defun elpamr-create-mirror-for-installed (&optional output-directory recreate-directory)
   "Export INSTALLED packages into a new directory.
 Create the html files for the mirror site.
-`elpamr-default-output-directory' is output directory if non-nil.
-Or else, user will be asked to provide the output directory."
+
+The first valid directory found from the below list
+will be used as mirror package's output directory:
+1. Argument: OUTPUT-DIRECTORY
+2. Variable: `elpamr-default-output-directory'
+3. Ask user to provide.
+
+When RECREATE-DIRECTORY is non-nil, OUTPUT-DIRECTORY
+will be deleted and recreated."
   (interactive)
   (let* (item
          final-pkg-list
@@ -271,41 +197,49 @@ Or else, user will be asked to provide the output directory."
       (if item (push item final-pkg-list)))
 
     ;; set output directory
-    (unless (and elpamr-default-output-directory (file-directory-p elpamr-default-output-directory))
-      (setq elpamr-default-output-directory (read-directory-name "Output directory:")))
+    (setq output-directory
+          (cond ((and output-directory
+                      (stringp output-directory))
+                 (file-name-as-directory output-directory))
+                ((and elpamr-default-output-directory
+                      (stringp elpamr-default-output-directory))
+                 (file-name-as-directory elpamr-default-output-directory))
+                (t (read-directory-name "Output directory:"))))
+
+    ;; Delete output directory if we need a clean output directory
+    (when (and recreate-directory
+               (file-directory-p output-directory))
+      (message "Re-create %s" output-directory)
+      (delete-directory output-directory t))
+
+    ;; Create output directory if it is not exist.
+    (unless (file-directory-p output-directory)
+      (make-directory output-directory t))
 
     (when (and (> (length final-pkg-list) 0)
-               elpamr-default-output-directory
-               (file-directory-p elpamr-default-output-directory))
+               output-directory
+               (file-directory-p output-directory))
 
       (dolist (dir dirs)
         (unless (or (member dir '("archives" "." ".."))
                     (not (setq pkg-info (elpamr--extract-info-from-dir dir))))
-          (cond
-           ;; copy single el
-           ((elpamr--is-single-el-by-name (car pkg-info) final-pkg-list)
-            (setq tar-cmd (concat (elpamr--executable-find "cp")
-                                  " "
-                                  (elpamr--input-fullpath (concat (file-name-as-directory dir) (car pkg-info) ".el"))
-                                  " "
-                                  (elpamr--output-fullpath (concat dir ".el")))))
-           ;; create tar using GNU tar
+          ;; create tar using GNU tar
            ;; BSD tar need set environment variable COPYFILE_DISABLE
-           (t
-            (setq tar-cmd (concat (if (elpamr--is-mac) "COPYFILE_DISABLE=\"\" " "")
-                                  (elpamr--executable-find "tar")
-                                  " cf "
-                                  (elpamr--output-fullpath dir) ".tar --exclude=\"*.elc\" --exclude=\"*~\" "
-                                  " -C "
-                                  package-user-dir
-                                  " "
-                                  dir))))
+          (setq tar-cmd (concat (if (elpamr--is-mac) "COPYFILE_DISABLE=\"\" " "")
+                                (elpamr--executable-find "tar")
+                                " cf "
+                                (elpamr--fullpath output-directory dir)
+                                ".tar --exclude=\"*.elc\" --exclude=\"*~\" "
+                                " -C "
+                                package-user-dir
+                                " "
+                                dir))
 
           ;; for windows
           (if elpamr-debug (message "tar-cmd=%s" tar-cmd))
           (shell-command tar-cmd)
           (setq cnt (1+ cnt))
-          (message "Creating *.tar and *.el ... %d%%" (/ (* cnt 100) len))))
+          (message "Creating *.tar ... %d%%" (/ (* cnt 100) len))))
 
       ;; output archive-contents
       (with-temp-buffer
@@ -316,8 +250,10 @@ Or else, user will be asked to provide the output directory."
             ;; each package occupies one line
             (insert (elpamr--one-item-for-archive-contents final-pkg)))
           (insert ")"))
-        (write-file (elpamr--output-fullpath "archive-contents" t)))
-      (message "DONE! Output into %s" elpamr-default-output-directory))))
+        (write-file (elpamr--fullpath output-directory
+                                      "archive-contents" t)))
+      (run-hook-with-args 'elpamr-finished-hook output-directory)
+      (message "DONE! Output into %s" output-directory))))
 
 (provide 'elpa-mirror)
 ;;; elpa-mirror.el ends here
