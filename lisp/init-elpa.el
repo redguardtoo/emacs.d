9ec4a80a375c86b015b4fac81282cdddff0cb73f
Chen Bin <chenbin.sh@gmail.com>
2018-05-10 00:05:29 +1000

    emms v5 need seq

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -314,7 +314,10 @@
 (require-package 'adoc-mode) ; asciidoc files
 (require-package 'magit)
 ;; {{ @see https://pawelbx.github.io/emacs-theme-gallery/
-(when *emacs24* (require-package 'color-theme))
+(when *emacs24*
+  (require-package 'color-theme)
+  ;; emms v5.0 need seq
+  (require-package 'seq))
 (when *emacs25*
   (require-package 'zenburn-theme)
   (require-package 'color-theme-sanityinc-solarized)
