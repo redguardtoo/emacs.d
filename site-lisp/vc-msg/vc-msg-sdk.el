813f92f4f6b6a839957ff2aba775da6395fd16d8
Chen Bin <chenbin.sh@gmail.com>
2018-06-05 11:01:54 +1000

    upgrade vc-msg

--- site-lisp/vc-msg/vc-msg-sdk.el
+++ site-lisp/vc-msg/vc-msg-sdk.el
@@ -8,7 +8,7 @@
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
+;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 
 ;; This program is distributed in the hope that it will be useful,
@@ -151,6 +151,6 @@ PATTERN itself is not part of summary."
                                             summary))
     ;; strip summary
     (vc-msg-sdk-trim summary)))
-;;; vc-msg-sdk.el ends here
 
 (provide 'vc-msg-sdk)
+;;; vc-msg-sdk.el ends here
