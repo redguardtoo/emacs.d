03e318ed6aa48be3d48aac6acbfa1a84d0040c8f
whatacold <whatacold@gmail.com>
2018-06-12 22:42:34 +0800

    Install yasnippet-snippets seperately

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -269,6 +269,7 @@
 (require-package 'git-link)
 (require-package 'cliphist)
 (require-package 'yasnippet)
+(require-package 'yasnippet-snippets)
 (require-package 'company)
 (require-package 'company-c-headers)
 (require-package 'company-statistics)
