65daef3f6a6fa4ad742ae857d58bbdbe3bac41a5
Chen Bin <chenbin.sh@gmail.com>
2018-07-25 13:10:29 +1000

    optimize ivy windows performance

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -273,7 +273,7 @@ If N is nil, use `ivy-mode' to browse the `kill-ring'."
 
 ;; better performance on everything (especially windows), ivy-0.10.0 required
 ;; @see https://github.com/abo-abo/swiper/issues/1218
-(setq ivy-dynamic-exhibit-delay-ms 200)
+(setq ivy-dynamic-exhibit-delay-ms 250)
 
 ;; Press C-p and Enter to select current input as candidate
 ;; https://oremacs.com/2017/11/30/ivy-0.10.0/
