6e180a4279628dc7bad44221712b078680cbfa72
Chen Bin <chenbin.sh@gmail.com>
2018-09-26 23:39:07 +1000

    test on Emacs 24.4

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -118,7 +118,7 @@
         ))
 
 ;; Un-comment below line if you follow "Install stable version in easiest way"
-;; (setq package-archives '(("myelpa" . "~/projs/myelpa")))
+;; (setq package-archives '(("localelpa" . "~/.emacs.d/localelpa/") ("myelpa" . "~/projs/myelpa/")))
 
 ;;------------------------------------------------------------------------------
 ;; Internal implementation, newbies should NOT touch code below this line!
