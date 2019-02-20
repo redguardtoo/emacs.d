4e49d81056c9cdc4bc053b4675daca78164a5b03
Chen Bin <chenbin.sh@gmail.com>
2019-02-20 12:34:27 +1100

    improve regex to grep component

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -499,7 +499,8 @@ If the character before and after CH is space or tab, CH is NOT slash"
                ((= n 1)
                 ;; grep references of current web component
                 ;; component could be inside styled-component like `const c = styled(Comp1)`
-                (counsel-etags-grep (format "(<|styled\\\()%s" (file-name-base buffer-file-name))))
+                (let* ((fb (file-name-base buffer-file-name)))
+                  (counsel-etags-grep (format "(<%s *$|styled\\\(%s\\))" fb fb))))
                ((= n 2)
                 ;; grep web component attribute name
                 (counsel-etags-grep (format "^ *%s[=:]" (or (thing-at-point 'symbol)
