f6e5809166c95ea133deddea86841b42a3d4fa6c
Chen Bin <chenbin.sh@gmail.com>
2019-06-21 15:48:30 +1000

    get most popular color themes

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -12,8 +12,7 @@
 ;; Please add the package name into `melpa-include-packages'
 ;; if it's not visible after  `list-packages'.
 (defvar melpa-include-packages
-  '(color-theme ; emacs24 need this package
-    ace-window ; lastest stable is released on year 2014
+  '(ace-window ; lastest stable is released on year 2014
     auto-package-update
     nov
     bbdb
@@ -42,28 +41,38 @@
     ;; counsel
     ;; swiper
     ;; }}
-    moe-theme
+    wgrep
+    ;; {{ themes in melpa unstable
     ample-theme
     molokai-theme
-    alect-themes
+    spacemacs-theme
+    leuven-theme
+    sublime-themes
     tangotango-theme
-    gruber-darker-theme
-    ample-zen-theme
-    flatland-theme
-    clues-theme
     darkburn-theme
+    ujelly-theme
+    afternoon-theme
+    organic-green-theme
+    inkpot-theme
+    flatui-theme
+    hc-zenburn-theme
+    naquadah-theme
+    seti-theme
+    spacegray-theme
+    jazz-theme
+    espresso-theme
+    phoenix-dark-pink-theme
+    tango-plus-theme
+    twilight-theme
+    minimal-theme
+    noctilux-theme
     soothe-theme
-    dakrone-theme
-    busybee-theme
-    bubbleberry-theme
-    cherry-blossom-theme
     heroku-theme
     hemisu-theme
     badger-theme
     distinguished-theme
-    challenger-deep-theme
     tao-theme
-    wgrep
+    ;; }}
     slime
     groovy-mode
     ;; company ; I won't wait another 2 years for stable
@@ -350,36 +359,63 @@ PACKAGE is a symbol, VERSION is a vector as produced by `version-to-list', and
 
 (when *emacs25*
   (require-package 'magit) ; Magit 2.12 is the last feature release to support Emacs 24.4.
+  ;; Most popular thems from https://emacsthemes.com/popular/index.html
   (require-package 'zenburn-theme)
-  (require-package 'color-theme-sanityinc-solarized)
+  (require-package 'solarized-theme)
+  (require-package 'spacemacs-theme)
   (require-package 'color-theme-sanityinc-tomorrow)
   (require-package 'monokai-theme)
-  (require-package 'molokai-theme) ; recommended
+  (require-package 'leuven-theme)
+  (require-package 'color-theme-sanityinc-solarized)
+  (require-package 'material-theme)
+  (require-package 'gruvbox-theme)
   (require-package 'moe-theme)
   (require-package 'cyberpunk-theme) ; recommended
   (require-package 'ample-theme)
-  (require-package 'gotham-theme)
-  (require-package 'gruvbox-theme)
+  (require-package 'dracula-theme) ; recommended
   (require-package 'alect-themes)
+  (require-package 'sublime-themes)
+  (require-package 'darktooth-theme)
+  (require-package 'anti-zenburn-theme)
+  (require-package 'gotham-theme)
+  (require-package 'apropospriate-theme)
+  (require-package 'tao-theme)
+  (require-package 'ujelly-theme)
+  (require-package 'molokai-theme) ; recommended
   (require-package 'grandshell-theme)
   (require-package 'tangotango-theme)
+  (require-package 'afternoon-theme)
   (require-package 'gruber-darker-theme)
   (require-package 'ample-zen-theme)
+  (require-package 'doom-themes)
+  (require-package 'noctilux-theme)
   (require-package 'flatland-theme)
+  (require-package 'organic-green-theme)
+  (require-package 'inkpot-theme)
+  (require-package 'flatui-theme)
+  (require-package 'hc-zenburn-theme)
   (require-package 'clues-theme)
   (require-package 'darkburn-theme) ; recommended
-  (require-package 'dracula-theme) ; recommended
   (require-package 'soothe-theme)
+  (require-package 'subatomic-theme)
+  (require-package 'naquadah-theme)
+  (require-package 'seti-theme)
+  (require-package 'spacegray-theme)
+  (require-package 'jazz-theme)
   (require-package 'dakrone-theme)
+  (require-package 'espresso-theme)
+  (require-package 'phoenix-dark-pink-theme)
+  (require-package 'tango-plus-theme)
   (require-package 'busybee-theme)
+  (require-package 'twilight-theme)
+  (require-package 'minimal-theme)
   (require-package 'bubbleberry-theme)
   (require-package 'cherry-blossom-theme)
   (require-package 'heroku-theme)
   (require-package 'hemisu-theme)
   (require-package 'badger-theme)
   (require-package 'distinguished-theme)
-  (require-package 'challenger-deep-theme)
-  (require-package 'tao-theme))
+  (require-package 'challenger-deep-theme))
 ;; }}
 
 ;; kill buffer without my confirmation
