761b1e8629fcde08495c17281bbe9e9676c7aef4
Chen Bin <chenbin.sh@gmail.com>
2019-11-19 12:21:59 +1100

    optimize ediff

--- lisp/init-ediff.el
+++ lisp/init-ediff.el
@@ -1,7 +1,14 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
+
 (defvar my-ediff-panel-name nil)
 
 (when (and (boundp 'startup-now) startup-now)
+
+  ;; remove `org-mode' from `auto-mode-alist'. So nodes in org file do NOT collapse at all
+  (setq auto-mode-alist  (rassq-delete-all 'org-mode auto-mode-alist))
+  ;; associate simpler major mode with org file instead
+  (add-auto-mode 'outline-mode "\\.org\\(_archive\\)?$")
+
 
   (defmacro my-ediff-command (cmd &optional no-arg)
     `(lambda (&optional arg)
@@ -34,7 +41,7 @@
     "p" (my-ediff-command 'ediff-previous-difference)
     "r" (my-ediff-command 'ediff-restore-diff-in-merge-buffer)
     "R" (my-ediff-command 'ediff-revert-buffers-then-recompute-diffs) ; press "1-space-R" to revert without confirmation
-    "xc" (lambda () (interactive) (save-buffers-kill-terminal t)) ; same keybinding as default setup
+    "xa" (lambda () (interactive) (save-buffers-kill-terminal t)) ; similar to vim
     ;; use 1 3 as hotkey to be consistent with vim
     "1" (my-ediff-command 'ediff-copy-A-to-C)
     "3" (my-ediff-command 'ediff-copy-B-to-C)
