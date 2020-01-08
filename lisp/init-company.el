5b010e8b44d7b41d3e48131bbfaaf4c13fc1f44a
Chen Bin <chenbin.sh@gmail.com>
2020-01-08 22:15:33 +1100

    company ctags setup

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -19,13 +19,13 @@
      ;; can't work with TRAMP
      (setq company-backends (delete 'company-ropemacs company-backends))
 
-     ;; company-ctags is faster than company-etags
+     ;; company-ctags is much faster out of box. No further optimiation needed
      (require 'company-ctags)
      (company-ctags-auto-setup)
 
      ;; (setq company-backends (delete 'company-capf company-backends))
 
-     ;; I don't like the downcase word in company-dabbrev!
+     ;; I don't like the downcase word in company-dabbrev
      (setq company-dabbrev-downcase nil
            ;; make previous/next selection in the popup cycles
            company-selection-wrap-around t
@@ -37,13 +37,12 @@
            company-idle-delay 0.2
            company-clang-insert-arguments nil
            company-require-match nil
-           company-etags-ignore-case t
+           company-ctags-ignore-case t ; I use company-ctags instead
            ;; @see https://github.com/company-mode/company-mode/issues/146
            company-tooltip-align-annotations t)
 
      ;; @see https://github.com/redguardtoo/emacs.d/commit/2ff305c1ddd7faff6dc9fa0869e39f1e9ed1182d
      (defadvice company-in-string-or-comment (around company-in-string-or-comment-hack activate)
-       ;; you can use (ad-get-arg 0) and (ad-set-arg 0) to tweak the arguments
        (if (memq major-mode '(php-mode html-mode web-mode nxml-mode))
            (setq ad-return-value nil)
          ad-do-it))
@@ -108,19 +107,5 @@
 ;; So we should NOT turn on company-ispell
 (add-hook 'org-mode-hook 'company-ispell-setup)
 ;; }}
-
-;; Wait 30  minutes to update cache from tags file
-;; assume `company-dabbrev-code' or `company-dabbrev' in the same group provides candidates
-;; @see https://github.com/company-mode/company-mode/pull/877 for tech details
-;; The purpose is to avoid rebuilding candidate too frequently because rebuilding could take
-;; too much time.
-(defvar company-etags-update-interval 1800
-  "The interval (seconds) to update candidate cache.
-Function `tags-completion-table' sets up variable `tags-completion-table'
-by parsing tags files.
-The interval stops the function being called too frequently.")
-
-(defvar company-etags-timer nil
-  "Timer to avoid calling function `tags-completion-table' too frequently.")
 
 (provide 'init-company)
