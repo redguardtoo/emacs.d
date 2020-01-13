c17e0ec71a9dbbd3ce6af56108f78b92b1273d37
Chen Bin <chenbin.sh@gmail.com>
2020-01-13 13:13:43 +1100

    suspend lsp for some mins when paste big string

--- lisp/init-clipboard.el
+++ lisp/init-clipboard.el
@@ -114,6 +114,13 @@ If N is 4, rectangle paste. "
       ;; turn off syntax highlight
       (font-lock-mode -1))
 
+    ;; past a big string, stop lsp temporarily
+    (when (and (> (length str) 1024)
+               (boundp 'lsp-mode)
+               lsp-mode)
+      (lsp-disconnect)
+      (run-at-time 300 nil  #'lsp-deferred))
+
     ;; paste after the cursor in evil normal state
     (cond
      ((not n)) ; do nothing
