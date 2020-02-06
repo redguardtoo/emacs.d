7a382c958550ad018f46f068631108aa5082f768
Chen Bin <chenbin.sh@gmail.com>
2020-02-06 23:52:54 +1100

    clean html snippets

--- snippets/rhtml-mode/script/script-angular.js
+++
@@ -1,6 +0,0 @@
-# -*- mode: snippet -*-
-# name: <script src="angular.js">...</script>
-# key: script
-# contributor: Chen Bin <chenbin DOT sh AT gmail>
-# --
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular${1:.min}.js"></script>
