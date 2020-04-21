0f95417d67d78f72b3abf645d2e7ce426a01573d
Chen Bin <chenbin.sh@gmail.com>
2020-04-21 11:07:03 +1000

    spell check dictionary setup

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -5,6 +5,9 @@
   "Check doublon (double word) when calling `flyspell-highlight-incorrect-region'.")
  (make-variable-buffer-local 'flyspell-check-doublon)
 
+(defvar my-default-spell-check-language "en_US"
+  "Language used by aspell and hunspell CLI.")
+
 (with-eval-after-load 'flyspell
   ;; {{ flyspell setup for web-mode
   (defun web-mode-flyspell-verify ()
@@ -63,14 +66,15 @@
 ;; @see http://lists.gnu.org/archive/html/aspell-announce/2011-09/msg00000.html
 (defun flyspell-detect-ispell-args (&optional run-together)
   "If RUN-TOGETHER is true, spell check the CamelCase words.
-Please note RUN-TOGETHER will make aspell less capable. So it should only be used in prog-mode-hook."
+Please note RUN-TOGETHER makes aspell less capable.  So it should be used in `prog-mode-hook' only."
   (let* (args)
     (when ispell-program-name
       (cond
        ;; use aspell
        ((string-match "aspell$" ispell-program-name)
         ;; force the English dictionary, support Camel Case spelling check (tested with aspell 0.6)
-        (setq args (list "--sug-mode=ultra" "--lang=en_US"))
+        ;; For aspell's option "--lang", "two letter ISO 3166 country code after a underscore" is OPTIONAL.
+        (setq args (list "--sug-mode=ultra" (format "--lang=%s" my-default-spell-check-language)))
         ;; "--run-together-min" could not be 3, see `check` in "speller_impl.cpp".
         ;; The algorithm is not precise.
         ;; Run `echo tasteTableConfig | aspell --lang=en_US -C --run-together-limit=16  --encoding=utf-8 -a` in shell.
@@ -98,17 +102,26 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
 ;;
 ;; Hunspell Setup:
 ;; 1. Install hunspell from http://hunspell.sourceforge.net/
+;;
 ;; 2. Download openoffice dictionary extension from
 ;; http://extensions.openoffice.org/en/project/english-dictionaries-apache-openoffice
-;; 3. Say `dict-en.oxt' is downloaded. Rename it to `dict-en.zip' and unzip
-;; the contents to a temporary folder.
-;; 4. Copy `en_US.dic' and `en_US.aff' from there to a folder where you
-;; save dictionary files. I use "~/usr_local/share/hunspell/".
-;; 5. Add that folder to shell environment variable `DICPATH'
-;; 6. Restart emacs so when hunspell is run by ispell/flyspell, that env
-;; variable is effective.
 ;;
-;; hunspell searches a dictionary named `en_US' in the path specified by
+;; 3. Say `dict-en.oxt' is downloaded. Rename it to `dict-en.zip' and unzip
+;; the contents to a temporary folder. Got "en_US.dic" and "en_US.aff" in
+;; that folder.
+;;
+;; 4. Hunspell's option "-d en_US" means finding dictionary "en_US"
+;; Set `ispell-local-dictionary-alist' to set that option of hunspell
+;;
+;; 5. Copy "en_US.dic" and "en_US.aff" from that temporary folder to
+;; the place for dictionary files. I use "~/usr_local/share/hunspell/".
+;;
+;; 6. Add that folder to shell environment variable "DICPATH"
+;;
+;; 7. Restart emacs so when hunspell is run by ispell/flyspell to make
+;; DICPATH take effect
+;;
+;; hunspell searches a dictionary named "en_US" in the path specified by
 ;; `$DICPATH' by default.
 
 (defvar my-force-to-use-hunspell nil
@@ -122,9 +135,9 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
  ;; use hunspell
  ((executable-find "hunspell")
   (setq ispell-program-name "hunspell")
-  (setq ispell-local-dictionary "en_US")
+  (setq ispell-local-dictionary "hunspelldict")
   (setq ispell-local-dictionary-alist
-        '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8))))
+        (list (list "hunspelldict" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil (list "-d" my-default-spell-check-language) nil 'utf-8))))
  (t (setq ispell-program-name nil)
     (message "You need install either aspell or hunspell for ispell")))
 
@@ -132,7 +145,7 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
 ;; when (ispell-send-string). Useless!
 ;; `ispell-extra-args' is *always* used when start CLI aspell process
 (setq-default ispell-extra-args (flyspell-detect-ispell-args t))
-;; (setq ispell-cmd-args (flyspell-detect-ispell-args))
+
 (defadvice ispell-word (around my-ispell-word activate)
   (let* ((old-ispell-extra-args ispell-extra-args))
     (ispell-kill-ispell t)
@@ -166,7 +179,7 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
     ;; I don't use flyspell in text-mode because I often use Chinese.
     ;; I'd rather manually spell check the English text
     (flyspell-mode 1)
-    ;; The only reason is to load turn onf and turn of `flyspell-mode' is load some
+    ;; The purpose to turn on and turn off `flyspell-mode' is to load some
     ;; major mode's own predicate
     (if turn-off (flyspell-mode -1))))
 
@@ -230,7 +243,7 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
                           (string-match "^=(" th)
                           (string-match ")=$" th)
                           (string= "w3m" th)))
-                 ;; embedded cde like =w3m= or org-link [[http://google.com][google]] or [[www.google.com]]
+                 ;; embedded code like =code= or org-link [[http://google.com][google]] or [[www.google.com]]
                  ;; langtool could finish checking before major mode prepare font face for all texts
                  (setq rlt nil))
                 (t
@@ -243,6 +256,15 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
 ;; }}
 
 (with-eval-after-load 'wucuo
+  ;; {{ wucuo is used to check camel cased code.  Code is usually written
+  ;; in English. If your code uses other language (Spanish?),
+  ;; Un-comment and modify below two lines:
+
+  ;; (setq wucuo-aspell-language-to-use "en")
+  ;; (setq wucuo-hunspell-dictionary-base-name "en_US")
+
+  ;; }}
+
   ;; do NOT turn on `flyspell-mode' automatically.
   ;; use `flyspell-buffer' instead
   (setq wucuo-flyspell-start-mode "lite")
