7408f5b1d1ba50890853fcc04f662914708de2c7
Chen Bin <chenbin.sh@gmail.com>
2020-04-23 10:29:40 +1000

    clean code

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 ;; Without this comment emacs25 adds (package-initialize) here
 ;; (package-initialize)
 
-(let* ((minver "24.4"))
+(let* ((minver "25.1"))
   (when (version< emacs-version minver)
     (error "Emacs v%s or higher is required." minver)))
 
@@ -98,7 +98,6 @@
   (require-init 'init-exec-path t) ;; Set up $PATH
   ;; Any file use flyspell should be initialized after init-spelling.el
   (require-init 'init-spelling t)
-  (require-init 'init-gui-frames t)
   (require-init 'init-uniquify t)
   (require-init 'init-ibuffer t)
   (require-init 'init-ivy)
--- lisp/init-essential.el
+++ lisp/init-essential.el
@@ -199,4 +199,25 @@ If OTHER-SOURCE is 2, get keyword from `kill-ring'."
 ;; (setq tramp-ssh-controlmaster-options "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
 ;; }}
 
+;; {{ GUI frames
+;; Suppress GUI features
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-echo-area-message t)
+
+;; Show a marker in the left fringe for lines not in the buffer
+(setq indicate-empty-lines t)
+
+;; NO tool bar
+(if (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+;; no scroll bar
+(if (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
+;; no menu bar
+(if (fboundp 'menu-bar-mode)
+  (menu-bar-mode -1))
+;; }}
+
 (provide 'init-essential)
