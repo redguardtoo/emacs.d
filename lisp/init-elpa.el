f4b2d8bcccdee0702807265eecc5002c715eb882
Chen Bin <chenbin.sh@gmail.com>
2020-05-26 13:06:13 +1000

    package-lint need be installable

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -27,6 +27,7 @@
     edit-server ; use Emacs to edit textarea in browser, need browser addon
     vimrc-mode
     rjsx-mode ; fixed the indent issue in jsx
+    package-lint ; for melpa pull request only
     auto-yasnippet
     typescript-mode ; the stable version lacks important feature (highlight function names)
     websocket ; to talk to the browser
