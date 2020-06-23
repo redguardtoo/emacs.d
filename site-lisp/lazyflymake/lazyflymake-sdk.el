35ce930a0dfe356ce0b43ceb3a854900594a701f
Chen Bin <chenbin.sh@gmail.com>
2020-06-23 18:43:21 +1000

    ugprade lazyflymake

--- site-lisp/lazyflymake/lazyflymake-sdk.el
+++ site-lisp/lazyflymake/lazyflymake-sdk.el
@@ -19,14 +19,18 @@
 
 (defun lazyflymake-sdk-code-file ()
   "Get code file to check."
-  (cond
-   ((and (lazyflymake-sdk-file-exist-p)
-         (not (buffer-narrowed-p)))
-    ;; save a little resource to create temp file
-    buffer-file-name)
-   (t
-    (flymake-init-create-temp-buffer-copy
-     'flymake-create-temp-inplace))))
+  (let* ((rlt (cond
+               ((and (lazyflymake-sdk-file-exist-p)
+                     (not (buffer-narrowed-p)))
+                ;; save a little resource to create temp file
+                buffer-file-name)
+               (t
+                (flymake-init-create-temp-buffer-copy
+                 'flymake-create-temp-inplace)))))
+    ;; convert absolute path on Windows to relative path
+    (setq rlt (file-relative-name rlt))
+    (if lazyflymake-debug (message "lazyflymake-sdk-code-file => %s" rlt))
+    rlt))
 
 (provide 'lazyflymake-sdk)
 ;;; lazyflymake-sdk.el ends here
