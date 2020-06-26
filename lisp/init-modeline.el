a9fc3efd843acb7a330bef35ce4da1ede301cf8c
Chen Bin <chenbin.sh@gmail.com>
2020-06-27 09:41:34 +1000

    show buffer file encoding in mode line

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -7,23 +7,26 @@
 (setq-default mode-line-format
   (list
     ;; the buffer name; the file name as a tool tip
-    '(:eval (propertize "%b " 'face nil
-        'help-echo (buffer-file-name)))
+    '(:eval (propertize "%b " 'face nil 'help-echo (buffer-file-name)))
 
     ;; line and column
     "(" ;; '%02' to set to 2 chars at least; prevents flickering
     "%02l" "," "%01c"
-      ;; (propertize "%02l" 'face 'font-lock-type-face) ","
-      ;; (propertize "%02c" 'face 'font-lock-type-face)
     ") "
 
-    ;; the current major mode for the buffer.
+    ;; @see https://www.gnu.org/software/emacs/manual/html_node/emacs/Help-Echo.html
     "["
+    ;; the current major mode for the buffer.
+    '(:eval (propertize "%m" 'face nil 'help-echo buffer-file-coding-system))
 
-    '(:eval (propertize "%m" 'face nil
-              'help-echo buffer-file-coding-system))
     " "
-
+    ;; buffer file encoding
+    '(:eval (let ((sys (coding-system-plist buffer-file-coding-system)))
+              (if (memq (plist-get sys :category)
+                        '(coding-category-undecided coding-category-utf-8))
+                  "UTF-8"
+                (upcase (symbol-name (plist-get sys :name))))))
+    " "
 
     ;; insert vs overwrite mode, input-method in a tooltip
     '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
@@ -39,9 +42,7 @@
 
     ;; is this buffer read-only?
     '(:eval (when buffer-read-only
-              (concat ","  (propertize "RO"
-                             'face nil
-                             'help-echo "Buffer is read-only"))))
+              (concat ","  (propertize "RO" 'face nil 'help-echo "Buffer is read-only"))))
     "] "
 
     ;;global-mode-string, org-timer-set-timer in org-mode need this
@@ -54,4 +55,3 @@
     ))
 
 (provide 'init-modeline)
-
