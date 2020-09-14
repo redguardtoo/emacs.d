4a2b4ffd5472b92862fe4168e27a10d45d916d64
Chen Bin <chenbin.sh@gmail.com>
2020-09-15 00:22:13 +1000

    add lazyflymake missing eslint file

---
+++ site-lisp/lazyflymake/lazyflymake-eslint.el
@@ -0,0 +1,27 @@
+;;; lazyflymake-eslint.el --- flymake eslint syntax checker  -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(require 'lazyflymake-sdk)
+
+(defun lazyflymake-eslint-err-line-pattern ()
+  "Return error line pattern.
+If return a list containing the pattern, `flymake-err-line-patterns' uses the
+list and is also converted into a buffer local variable.
+If return the pattern, it's is pushed to `flymake-err-line-patterns'.
+If return nil, nothing need be done."
+  nil)
+
+(defun lazyflymake-eslint-init ()
+  "Eslint syntax check init."
+  (let* ((dir (locate-dominating-file default-directory "node_modules"))
+         (program (if dir (concat dir "node_modules/eslint") "eslint")))
+    (when (executable-find program)
+      (list program
+            (list "-f unix" (lazyflymake-sdk-code-file))))))
+
+(provide 'lazyflymake-eslint)
+;;; lazyflymake-eslint.el ends here
