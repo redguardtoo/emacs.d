90bd50f5f2605c67206c0ea7d812980262cc15a8
Chen Bin <chenbin.sh@gmail.com>
2020-11-16 00:47:16 +1100

    optimize which-key-mode

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -1026,8 +1026,18 @@ might be bad."
   (browse-url-generic (concat "file://" (buffer-file-name))))
 
 ;; {{ which-key-mode
-(setq which-key-allow-imprecise-window-fit t) ; performance
-(setq which-key-separator ":")
+(defvar my-show-which-key-when-press-C-h nil)
+(with-eval-after-load 'which-key
+  (setq which-key-allow-imprecise-window-fit t) ; performance
+  (setq which-key-separator ":")
+  (setq which-key-idle-delay 1.5)
+  (when my-show-which-key-when-press-C-h
+    ;; @see https://twitter.com/bartuka_/status/1327375348959498240?s=20
+    ;; Therefore, the which-key pane only appears if I hit C-h explicitly.
+    ;; C-c <C-h> for example - by Wanderson Ferreira
+    (setq which-key-idle-delay 10000)
+    (setq which-key-show-early-on-C-h t))
+  (setq which-key-idle-secondary-delay 0.05))
 (my-run-with-idle-timer 2 #'which-key-mode)
 ;; }}
 
