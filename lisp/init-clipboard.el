8ea127c69cd6e5d6dbbe2c1dce91131c4a4c0cd2
Chen Bin <chenbin.sh@gmail.com>
2021-02-19 13:17:34 +1100

    clean code

--- lisp/init-clipboard.el
+++ lisp/init-clipboard.el
@@ -2,10 +2,10 @@
 
 ;; Use the system clipboard
 ;; @see https://www.emacswiki.org/emacs/CopyAndPaste
-;; So `C-y' could paste from clipbord if you are NOT using emacs-nox
+;; So `C-y' could paste from clipboard if you are NOT using emacs-nox
 ;; I only use `paste-from-x-clipboard', not `C-y'.
-(setq x-select-enable-clipboard t
-      x-select-enable-primary t)
+(setq x-select-enable-clipboard t)
+(setq x-select-enable-primary t)
 
 ;; kill-ring and clipboard are same? No, it's annoying!
 (setq save-interprogram-paste-before-kill nil)
@@ -54,9 +54,9 @@ If N is not nil, copy file name and line number."
                                 (message "%s%s" msg hint)))))
 
 (defun copy-to-x-clipboard (&optional num)
-  "If NUM equals 1, copy the downcased string.
-If NUM equals 2, copy the captalized string.
-If NUM equals 3, copy the upcased string.
+  "If NUM equals 1, copy the down-cased string.
+If NUM equals 2, copy the capitalized string.
+If NUM equals 3, copy the up-cased string.
 If NUM equals 4, indent 4 spaces."
   (interactive "P")
   (let* ((thing (my-use-selected-string-or-ask "")))
@@ -83,8 +83,8 @@ If NUM equals 4, indent 4 spaces."
   "Remove selected text and paste string clipboard.
 If N is 1, we paste diff hunk whose leading char should be removed.
 If N is 2, paste into `kill-ring' too.
-If N is 3, converted dashed to camelcased then paste.
-If N is 4, rectangle paste. "
+If N is 3, converted dashed to camel-cased then paste.
+If N is 4, rectangle paste."
   (interactive "P")
   (when (and (functionp 'evil-normal-state-p)
              (functionp 'evil-move-cursor-back)
@@ -97,8 +97,7 @@ If N is 4, rectangle paste. "
 
     (when (> (length str) (* 256 1024))
       ;; use light weight `major-mode' like `js-mode'
-      (when (derived-mode-p 'js2-mode)
-        (js-mode 1))
+      (when (derived-mode-p 'js2-mode) (js-mode 1))
       ;; turn off syntax highlight
       (font-lock-mode -1))
 
