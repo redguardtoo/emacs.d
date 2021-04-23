d34437a14bf09085cd657753ceffa8c8a2c0df71
Chen Bin <chenbin.sh@gmail.com>
2021-04-23 17:50:31 +1000

    my-hint-untracked-files should not run when cwd is home directory

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -402,21 +402,24 @@ If nothing is selected, use the word under cursor as function name to look up."
 
 
 (defun my-hint-untracked-files ()
-  "If untracked files and commited files share same extension, warn users."
-  (let* ((exts (mapcar 'file-name-extension (my-lines-from-command-output "git diff-tree --no-commit-id --name-only -r HEAD")))
-         (untracked-files (my-lines-from-command-output "git --no-pager ls-files --others --exclude-standard"))
-         (lookup-ext (make-hash-table :test #'equal))
-         rlt)
-    ;; file extensions of files in HEAD commit
-    (dolist (ext exts)
-      (puthash ext t lookup-ext))
-    ;; If untracked file has same file extension as committed files
-    ;; maybe they should be staged too?
-    (dolist (file untracked-files)
-      (when (gethash (file-name-extension file) lookup-ext)
-        (push (file-name-nondirectory file) rlt)))
-    (when rlt
-      (message "Stage files? %s" (mapconcat 'identity rlt " ")))))
+  "If untracked files and committed files share same extension, warn users."
+
+  ;; don't scan whole home directory
+  (unless (string= (file-truename default-directory) (file-truename "~/"))
+    (let* ((exts (mapcar 'file-name-extension (my-lines-from-command-output "git diff-tree --no-commit-id --name-only -r HEAD")))
+           (untracked-files (my-lines-from-command-output "git --no-pager ls-files --others --exclude-standard"))
+           (lookup-ext (make-hash-table :test #'equal))
+           rlt)
+      ;; file extensions of files in HEAD commit
+      (dolist (ext exts)
+        (puthash ext t lookup-ext))
+      ;; If untracked file has same file extension as committed files
+      ;; maybe they should be staged too?
+      (dolist (file untracked-files)
+        (when (gethash (file-name-extension file) lookup-ext)
+          (push (file-name-nondirectory file) rlt)))
+      (when rlt
+        (message "Stage files? %s" (mapconcat 'identity rlt " "))))))
 
 (with-eval-after-load 'magit
   (defun my-git-check-status ()
