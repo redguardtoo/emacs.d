22888393517bf12d6cb09f37ada6fa249d0ede7b
Chen Bin <chenbin.sh@gmail.com>
2021-04-25 22:02:47 +1000

    use pdf-view-goto-page if pdf-view-mode is used for pdf file

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -133,6 +133,20 @@ It's value could be customized liked \"/usr/bin/firefox\".
       (apply orig-func args)))
   (advice-add 'org-open-at-point :around #'my-org-open-at-point-hack)
 
+  (defun my-org-docview-open-hack (orig-func &rest args)
+    (let* ((link (car args)) path page)
+      (string-match "\\(.*?\\)\\(?:::\\([0-9]+\\)\\)?$" link)
+      (setq path (match-string 1 link))
+      (setq page (and (match-beginning 2)
+                 (string-to-number (match-string 2 link))))
+      (org-open-file path 1)
+      (when page
+        (cond
+         ((eq major-mode 'pdf-view-mode)
+          (pdf-view-goto-page page))
+         (t
+          (doc-view-goto-page page))))))
+  (advice-add 'org-docview-open :around #'my-org-docview-open-hack)
   (defun my-org-publish-hack (orig-func &rest args)
     "Stop running `major-mode' hook when `org-publish'."
     (let* ((my-load-user-customized-major-mode-hook nil))
