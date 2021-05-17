5d0c33e7cd6d988ec7aca645f3474ef705cecafa
Chen Bin <chenbin.sh@gmail.com>
2021-05-17 20:53:04 +1000

    ivy delay setup

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -310,7 +310,7 @@ If N is nil, use `ivy-mode' to browse `kill-ring'."
 (with-eval-after-load 'ivy
   ;; better performance on everything (especially windows), ivy-0.10.0 required
   ;; @see https://github.com/abo-abo/swiper/issues/1218
-  (setq ivy-dynamic-exhibit-delay-ms 250)
+  (setq ivy-dynamic-exhibit-delay-ms (if (or *unix* *is-a-mac*) 10 250))
 
   ;; Press C-p and Enter to select current input as candidate
   ;; https://oremacs.com/2017/11/30/ivy-0.10.0/
