64cc5b9d8876c6e6a7bae45b4253f8d60eb6d42d
Chen Bin <chenbin.sh@gmail.com>
2021-05-18 14:48:39 +1000

    doc on green energy

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -309,8 +309,10 @@ If N is nil, use `ivy-mode' to browse `kill-ring'."
 
 (with-eval-after-load 'ivy
   ;; better performance on everything (especially windows), ivy-0.10.0 required
+  ;; when `ivy-dynamic-exhibit-delay-ms' is a non-zero value
+  ;; Setting it to a bigger value in ALL OSs is also more green energy btw.
   ;; @see https://github.com/abo-abo/swiper/issues/1218
-  (setq ivy-dynamic-exhibit-delay-ms (if (or *unix* *is-a-mac*) 10 250))
+  (setq ivy-dynamic-exhibit-delay-ms (if (or *unix* *is-a-mac*) 0 250))
 
   ;; Press C-p and Enter to select current input as candidate
   ;; https://oremacs.com/2017/11/30/ivy-0.10.0/
