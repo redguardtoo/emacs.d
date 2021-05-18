4262308dc4ee2677598a00afc010f6d5c1cf6c3b
donney <donney@SH-tanghl-mac.local>
2021-05-18 13:53:12 +0800

    update ...

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -317,7 +317,7 @@ If N is nil, use `ivy-mode' to browse `kill-ring'."
 (with-eval-after-load 'ivy
   ;; better performance on everything (especially windows), ivy-0.10.0 required
   ;; @see https://github.com/abo-abo/swiper/issues/1218
-  (setq ivy-dynamic-exhibit-delay-ms 0)
+  (setq ivy-dynamic-exhibit-delay-ms (if (or *is-a-mac* *unix*) 10 250))
 
   ;; Press C-p and Enter to select current input as candidate
   ;; https://oremacs.com/2017/11/30/ivy-0.10.0/
