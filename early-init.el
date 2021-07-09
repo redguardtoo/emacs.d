c58362619d5f89a98ed7bdf71fd3cc195e158dff
Chen Bin <chenbin.sh@gmail.com>
2021-07-09 13:20:39 +1000

    optimize start speed

--- early-init.el
+++ early-init.el
@@ -4,3 +4,7 @@
           (boundp 'startup-now)
           noninteractive)
   (setq package-enable-at-startup nil))
+
+;; @see https://www.reddit.com/r/emacs/comments/ofhket/further_boost_start_up_time_with_a_simple_tweak/
+;; 10% speed up of startup for my configuration
+(setq gc-cons-threshold most-positive-fixnum)
