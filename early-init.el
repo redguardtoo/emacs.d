357057b151d642b308c5a82e23f59e5295f6869b
Chen Bin <chenbin.sh@gmail.com>
2021-09-24 16:03:14 +1000

    set up early-init.el

--- early-init.el
+++ early-init.el
@@ -8,3 +8,7 @@
 ;; @see https://www.reddit.com/r/emacs/comments/ofhket/further_boost_start_up_time_with_a_simple_tweak/
 ;; 10% speed up of startup for my configuration
 (setq gc-cons-threshold most-positive-fixnum)
+
+(setq inhibit-startup-message t
+      ;; speed startup, learned from doom emacs
+      initial-major-mode 'fundamental-mode)
