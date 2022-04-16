33773aff914441d73edc497b72314043866eaae0
Chen Bin <chenbin.sh@gmail.com>
2022-04-16 18:55:17 +1000

    respect Emacs builtin c/c++ mode setup

--- lisp/init-cc-mode.el
+++ lisp/init-cc-mode.el
@@ -1,20 +1,20 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
-;; avoid default "gnu" style, use more popular one
-(setq c-default-style '((java-mode . "java")
-                        (awk-mode . "awk")
-                        (other . "linux")))
+;; Tip 1: use more popular style "linux" instead of "gnu",
+;;
+;;   (setq c-default-style '((java-mode . "java")
+;;                           (awk-mode . "awk")
+;;                           (other . "linux")))
 
-(defun fix-c-indent-offset-according-to-syntax-context (key val)
-  ;; remove the old element
-  (setq c-offsets-alist (delq (assoc key c-offsets-alist) c-offsets-alist))
-  ;; new value
-  (add-to-list 'c-offsets-alist '(key . val)))
-
-(setq-default c-basic-offset 4)
+;; Tip 2: Search my article "C/C++/Java code indentation in Emacs"
+;; My code might be obsolete, but the knowledge is still valid.kk
+;;
+;; C code example:
+;;   if(1) // press ENTER here, zero means no indentation
+;;   void fn() // press ENTER here, zero means no indentation
 
 (defun my-common-cc-mode-setup ()
-  "setup shared by all languages (java/groovy/c++ ...)"
+  "Setup shared by all languages (java/groovy/c++ ...)."
   ;; give me NO newline automatically after electric expressions are entered
   (setq c-auto-newline nil)
 
@@ -24,29 +24,20 @@
   (setq lazy-lock-defer-time 0)
 
   ;make DEL take all previous whitespace with it
-  (c-toggle-hungry-state 1)
-
-  ;; indent
-  ;; google "C/C++/Java code indentation in Emacs" for more advanced skills
-  ;; C code:
-  ;;   if(1) // press ENTER here, zero means no indentation
-  (fix-c-indent-offset-according-to-syntax-context 'substatement 0)
-  ;;   void fn() // press ENTER here, zero means no indentation
-  (fix-c-indent-offset-according-to-syntax-context 'func-decl-cont 0))
+  (c-toggle-hungry-state 1))
 
 (defun my-c-mode-setup ()
   "C/C++ only setup."
   ;; @see http://stackoverflow.com/questions/3509919/ \
   ;; emacs-c-opening-corresponding-header-file
-  (local-set-key (kbd "C-x C-o") 'ff-find-other-file)
+  ;; Use `ff-find-other-file' to open C/C++ header
 
   (setq cc-search-directories '("." "/usr/include" "/usr/local/include/*" "../*/include" "$WXWIN/include"))
 
-  ;; @see https://github.com/redguardtoo/cpputils-cmake
   ;; In theory, you can write your own Makefile for `flymake-mode' without cmake.
   ;; Nobody actually does it in real world.
 
-  ;; debugging Emacs C code
+  ;; Browse Emacs' C code
   (push '(nil "^DEFUN *(\"\\([a-zA-Z0-9-]+\\)" 1) imenu-generic-expression )
 
   ;; make a #define be left-aligned
@@ -61,3 +52,4 @@
 (add-hook 'c-mode-common-hook 'c-mode-common-hook-setup)
 
 (provide 'init-cc-mode)
+;;; init-cc-mode.el ends here
