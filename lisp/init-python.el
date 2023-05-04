0320b87c8ce1d4cff510827deb28468500d1b2bd
Chen Bin <chenbin.sh@gmail.com>
2023-05-05 00:44:29 +1000

    python elpy setup

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -16,4 +16,37 @@
   ;; emacs 24.4+
   (setq electric-indent-chars (delq ?: electric-indent-chars)))
 
+(defvar my-python-venv-directories
+  '("~/.emacs.d/elpy/rpc-venv"
+    "~/.venv")
+  "Directories of python venv.")
+
+(defun my-activate-python-venv (directory)
+  "Activate python venv in DIRECTORY."
+  (my-ensure 'pyvenv)
+  (let* ((venv-dir directory)
+         (python pyvenv-virtualenvwrapper-python))
+
+    (setq python (and (executable-find python) (executable-find "python3")))
+    (cond
+     ((null python)
+      (message "Python executable \"%s\" is missing." python))
+     ((not (file-exists-p venv-dir))
+      (message "Directory \"%s\" is missing." venv-dir))
+     (t
+      (let* ((pyvenv-virtualenvwrapper-python python))
+        (pyvenv-activate venv-dir))))))
+
+(defun my-select-python-venv-and-restart-elpy ()
+  "Activate python venv."
+  (interactive)
+  (let* ((venv-dir (completing-read "Select Python venv: "
+                                    my-python-venv-directories)))
+    (when venv-dir
+      (my-activate-python-venv venv-dir)
+      (elpy-disable)
+      (elpy-shell-kill-all)
+      (elpy-enable))))
+
 (provide 'init-python)
+;;; init-python.el ends here
