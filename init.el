182b37a488f3e091b0f5754f79e92421b1be6482
Chen Bin <chenbin.sh@gmail.com>
2023-05-31 00:05:08 +1000

    support pdfgrep

--- init.el
+++ init.el
@@ -139,6 +139,7 @@
   (require-init 'init-hydra) ; hotkey is required everywhere
   ;; use evil mode (vi key binding)
   (require-init 'init-evil) ; init-evil dependent on init-clipboard
+  (require-init 'init-pdf)
 
   ;; ediff configuration should be last so it can override
   ;; the key bindings in previous configuration
